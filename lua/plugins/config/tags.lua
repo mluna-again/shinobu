972dd917c6ff9eb756ff83644d5fdc0dfdd8e264
Manuel Luna <manuellunada@gmail.com>
2022-02-05 00:38:20 -0600

    tags width

--- lua/plugins/config/tags.lua
+++ lua/plugins/config/tags.lua
@@ -7,7 +7,7 @@ return {
 			auto_preview = true,
 			position = "right",
 			relative_width = true,
-			width = 30,
+			width = 35,
 			auto_close = false,
 			show_numbers = false,
 			show_relative_numbers = false,
