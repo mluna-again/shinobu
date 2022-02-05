8fa63e5b271dae7cc8c3619738cb08b059cfbcca
Manuel Luna <manuellunada@gmail.com>
2022-02-05 00:35:54 -0600

    helpers

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -23,3 +23,26 @@ au! FileType * setlocal formatoptions-=c formatoptions-=r formatoptions -=0
 ]])
 vim.g.indentLine_fileTypeExclude = {'dashboard', 'dockerfile', 'json'}
 vim.opt.colorcolumn = "99999"
+
+-- COMMANDS
+vim.cmd([[
+command Ruby !ruby %
+command Lisp !sbcl --script %
+command Clojure !clj -M %
+command Node !node %
+command Python !python %
+command Nim !nim c -r %
+command Elixir !elixir %
+command Env :e .env
+command T :call ToggleFormatter()
+command C normal ggVG"+y
+command IEx :FloatermNew --name=iex --autoclose=1 iex -S mix
+command IRB :FloatermNew irb
+
+autocmd! BufEnter *.rb nmap <silent> <C-p> :Ruby<CR>
+autocmd! BufEnter *.lisp,*.cl nmap <silent> <C-p> :Lisp<CR>
+autocmd! BufEnter *.cls nmap <silent> <C-p> :Clojure<CR>
+autocmd! BufEnter *.js nmap <silent> <C-p> :Node<CR>
+autocmd! BufEnter *.nim nmap <silent> <C-p> :Nim<CR>
+autocmd! BufEnter *.ex,*.exs nmap <silent> <C-p> :Elixir<CR>
+]])
