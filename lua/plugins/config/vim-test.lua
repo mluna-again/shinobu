9e360f98c20993b75b493fc1be13150358341d2d
Manuel Luna <manuellunada@gmail.com>
2022-02-23 17:43:40 -0600

    test keybindings

--- lua/plugins/config/vim-test.lua
+++ lua/plugins/config/vim-test.lua
@@ -1,13 +1,14 @@
 require('core.maps')
 
-nmap("<Leader>r", ":TestNearest<CR>")
-nmap("<Leader>R", ":TestFile<CR>")
-nmap("<Leader>T", ":TestSuite<CR>")
-
 vim.cmd([[
 let test#strategy = "floaterm"
 ]])
 
 return {
-'vim-test/vim-test'
+	'vim-test/vim-test',
+	config = function()
+		nmap("<Leader>r", ":TestNearest<CR>")
+		nmap("<Leader>R", ":TestFile<CR>")
+		nmap("<Leader>T", ":TestSuite<CR>")
+	end
 }
