677e37b523257a4c1b424e2896f1f925dcc79aec
Manuel Luna <manuellunada@gmail.com>
2022-03-11 14:10:12 -0600

    tmux

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 # "Basic" packages
-sudo pacman -Syu --noconfirm neovim git nodejs fzf ripgrep zsh starship exa which python3 python-pip bat
+sudo pacman -Syu --noconfirm neovim git nodejs fzf ripgrep zsh starship exa which python3 python-pip bat tmux
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # Config neovim
 mkdir ~/.config
@@ -13,6 +14,10 @@ sudo chsh -s $(which zsh) vagrant
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &> /dev/null
 cp ~/.config/nvim/.zshrc ~
 cp ~/.config/nvim/starship.toml ~/.config
+
+# Config tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp ~/.config/nvim/.tmux.config ~
 
 cat <<EOF
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
