144938459d981332b859f3fec7db769d7bbc62c4
Manuel Luna <manuellunada@gmail.com>
2022-05-07 16:58:42 -0500

    elixirls cmd

--- lua/plugins/config/nvim-cmp.lua
+++ lua/plugins/config/nvim-cmp.lua
@@ -111,6 +111,7 @@ return {
 		local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 		require('lspconfig').elixirls.setup{
 			capabilities = capabilities,
+			cmd = { "language_server.sh" }
 			-- cmd = { string.format("%s/.local/bin/elixirls/elixir-ls/language_server.sh", os.getenv("HOME")) }
 		}
 		require('lspconfig').tsserver.setup{
