e3400e48cdbaec799becf9be62bdb2f66006b30f
Manuel Luna <manuellunada@gmail.com>
2022-05-24 01:23:58 -0500

    fugitive maps

---
+++ lua/plugins/config/fugitive.lua
@@ -0,0 +1,10 @@
+require('core.maps')
+
+return {
+	'tpope/vim-fugitive',
+	config = function ()
+		nmap("<leader>gd", "Gvdiffsplit!<CR>")
+		nmap("gdr", ":diffget //2<CR>")
+		nmap("gdt", ":diffget //2<CR>")
+	end
+}
