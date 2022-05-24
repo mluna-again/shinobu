af853d4f105a0e9852fcf0245608c534edbafbff
Manuel Luna <manuellunada@gmail.com>
2022-05-24 01:25:36 -0500

    fix map

--- lua/plugins/config/fugitive.lua
+++ lua/plugins/config/fugitive.lua
@@ -3,7 +3,7 @@ require('core.maps')
 return {
 	'tpope/vim-fugitive',
 	config = function ()
-		nmap("<leader>gd", "Gvdiffsplit!<CR>")
+		nmap("<leader>gd", ":Gvdiffsplit!<CR>")
 		nmap("gdr", ":diffget //2<CR>")
 		nmap("gdt", ":diffget //2<CR>")
 	end
