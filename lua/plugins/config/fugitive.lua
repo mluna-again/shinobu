8f693fb6b4305eddd3dede7fe5dd2cd9d9d5808e
Manuel Luna <manuellunada@gmail.com>
2022-05-24 01:39:42 -0500

    fugitive maps

--- lua/plugins/config/fugitive.lua
+++ lua/plugins/config/fugitive.lua
@@ -4,7 +4,8 @@ return {
 	'tpope/vim-fugitive',
 	config = function ()
 		nmap("<leader>gd", ":Gvdiffsplit!<CR>")
-		nmap("gdh", ":diffget //2<CR>")
-		nmap("gdl", ":diffget //3<CR>")
+		nmap("<leader>gt", ":Git mergetool<CR>")
+		nmap("gdj", ":diffget //2<CR>")
+		nmap("gdk", ":diffget //3<CR>")
 	end
 }
