fc11549d4de8fcdd507c474356da43979734b613
Manuel Luna <manuellunada@gmail.com>
2022-05-30 17:34:10 -0500

    new conjure mappings

--- lua/plugins/config/conjure.lua
+++ lua/plugins/config/conjure.lua
@@ -5,7 +5,9 @@ return {
 	config = function ()
 		nmap("<leader>cer", ":ConjureEvalRootForm<CR>")
 		nmap("<leader>cee", ":ConjureEvalCurrentForm<CR>")
-		nmap("<leader>clv", ":ConjureLogVSplit<CR>")
-		nmap("<leader>ceb", ":ConjureLogVSplit<CR>")
+		nmap("<leader>ceb", ":ConjureEvalBuf<CR>")
+		nmap("<leader>clv", ":ConjureLogVSplit<CR><C-w>x")
+		nmap("<leader>cls", ":ConjureLogSplit<CR>")
+		nmap("<leader>clr", ":ConjureLogResetSof<CR>")
 	end
 }
