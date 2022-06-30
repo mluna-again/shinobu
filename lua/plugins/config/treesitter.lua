e10a8dfe043ad3bfed6a31cb902b05e02dfa68ff
Manuel Luna <manuellunada@gmail.com>
2022-06-29 19:10:28 -0500

    kotlin 😑👍

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function ()
 		require'nvim-treesitter.configs'.setup {
 			-- A list of parser names, or "all"
-			ensure_installed = { "elixir", "typescript", "http", "json", "javascript", "ruby", "clojure", "c_sharp", "go" },
+			ensure_installed = { "elixir", "typescript", "http", "json", "javascript", "ruby", "clojure", "c_sharp", "go", "kotlin" },
 
 			-- Install parsers synchronously (only applied to `ensure_installed`)
 			sync_install = false,
