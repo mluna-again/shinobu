bb80cb48ed2c41ea4f808e435709e25c051673e3
Manuel Luna <manuellunada@gmail.com>
2022-07-09 14:20:54 -0500

    battery icon

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -8,8 +8,22 @@ percentage=$(upower -i $battery | grep -i percentage | awk '{print $2}')
 
 low_battery=$([ $(echo $percentage | cut -c -2) -le 25 ] && echo "yes" || echo "no")
 
-if [ "$low_battery" == "no" ]; then
-	echo "#[fg=green]$percentage"
+plugged=$(upower -i $battery | grep -i ".*state:.*\bcharging\b.*" &>/dev/null && echo "yes" || echo "no")
+
+if [ "$plugged" == "yes" ]; then
+	echo "#[fg=green] $percentage"
+elif [ "$low_battery" == "no" ]; then
+
+	if [ "$percentage" -ge 90 ]; then
+		echo "#[fg=green] $percentage"
+	elif [ "$percentage" -ge 70 ]; then
+		echo "#[fg=green] $percentage"
+	elif [ "$percentage" -ge 50 ]; then
+		echo "#[fg=green] $percentage"
+	elif [ "$percentage" -ge 30 ]; then
+		echo "#[fg=green] $percentage"
+	fi
+
 else
-	echo "#[fg=red]$percentage"
+	echo "#[fg=red] $percentage"
 fi
