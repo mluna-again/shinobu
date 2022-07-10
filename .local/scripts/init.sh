2425df49abfde19714e1c378090f867623584f19
Manuel Luna <manuellunada@gmail.com>
2022-07-10 12:17:55 -0500

    update init script

--- .local/scripts/init.sh
+++ .local/scripts/init.sh
@@ -13,26 +13,18 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install 16
 
-# Config neovim
-mkdir ~/.config
-git clone https://github.com/mluna711/shinobu.git ~/.config/nvim
+# neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 pip3 install neovim
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 nvim --headless -c 'autocmd VimEnter * quitall' -c 'echo "Help.\n"'
 
-# Config zsh
-sudo chsh -s $(which zsh) vagrant
+# zsh
+chsh -s $(which zsh) $(whoami)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &> /dev/null
-cp ~/.config/nvim/.zshrc ~
-cp ~/.config/nvim/starship.toml ~/.config
 
-# Config tmux
+# tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp ~/.config/nvim/.tmux.conf ~
-
-# Config alacritty
-cp -r ~/.config/nvim/alacritty ~/.config
 
 cat <<EOF
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
