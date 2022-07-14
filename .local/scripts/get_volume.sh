6942725b93bf5a7d5eadc5ff0ffa672bae3a84d1
Manuel Luna <manuellunada@gmail.com>
2022-07-14 12:45:03 -0500

    fix volume

--- .local/scripts/get_volume.sh
+++ .local/scripts/get_volume.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-amixer get Master | grep -e '[0-9]*%' | awk '{print $4}' | sed 's/\[//g' | sed 's/\]//g' | sed 's/%//g'
+amixer get Master | grep -e '[0-9]*%' | sed 1q | awk '{print $(NF - 1)}' | sed 's/\[//g' | sed 's/\]//g' | sed 's/%//g'
