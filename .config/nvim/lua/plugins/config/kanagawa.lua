a2b75c208cb2836355dccb9a7cc87f5a2ecc5ab7
Manuel Luna <manuellunada@gmail.com>
2022-07-22 15:51:20 -0500

    :,)

--- .config/nvim/lua/plugins/config/kanagawa.lua
+++ .config/nvim/lua/plugins/config/kanagawa.lua
@@ -8,6 +8,14 @@ return {
 		  autocmd ColorScheme * hi TelescopePromptCounter guibg=#16161D guifg=#C8C093
 		  autocmd ColorScheme * hi TelescopePromptNormal guibg=#16161D guifg=#C8C093
 		  autocmd ColorScheme * hi TelescopePromptTitle guibg=#C34043 guifg=#16161D
+
+		  autocmd ColorScheme * hi TelescopePreviewBorder guibg=#16161D guifg=#16161D
+		  autocmd ColorScheme * hi TelescopePreviewNormal guibg=#16161D guifg=#C8C093
+		  autocmd ColorScheme * hi TelescopePreviewTitle guibg=#957FB8 guifg=#16161D
+
+		  autocmd ColorScheme * hi TelescopeResultsBorder guibg=#16161D guifg=#16161D
+		  autocmd ColorScheme * hi TelescopeResultsNormal guibg=#16161D guifg=#C8C093
+		  autocmd ColorScheme * hi TelescopeResultsTitle guibg=#6A9589 guifg=#16161D
 		augroup end
 		]])
 		vim.cmd("colorscheme kanagawa")
