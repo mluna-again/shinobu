6558b039b016df84d1ba653a457773ed6a0671e2
Manuel Luna <manuellunada@gmail.com>
2022-07-22 15:39:12 -0500

    im going insane

--- .config/nvim/lua/plugins/config/kanagawa.lua
+++ .config/nvim/lua/plugins/config/kanagawa.lua
@@ -1,13 +1,15 @@
 return {
 	"rebelot/kanagawa.nvim",
 	config = function ()
+		vim.cmd([[
+		augroup Kanawaga
+			autocmd!
+		  autocmd ColorScheme * hi TelescopePromptBorder guibg=#16161D guifg=#16161D
+		  autocmd ColorScheme * hi TelescopePromptCounter guibg=#16161D guifg=#C8C093
+		  autocmd ColorScheme * hi TelescopePromptNormal guibg=#16161D guifg=#C8C093
+		  autocmd ColorScheme * hi TelescopePromptTitle guibg=#C34043 guifg=#16161D
+		augroup end
+		]])
 		vim.cmd("colorscheme kanagawa")
-		-- vim.cmd([[
-		-- augroup Kanawaga
-		-- 	autocmd!
-		--   autocmd ColorScheme * hi TelescopePromptBorder guibg=#C8C093 guifg=#C8C093
-		--   autocmd ColorScheme * hi TelescopePromptCounter guibg=#C8C093 guifg=#C8C093
-		-- augroup end
-		-- ]])
 	end
 }
