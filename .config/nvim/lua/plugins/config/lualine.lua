22d25beb3036405d99f6c31cdced1b5bc26cf0e4
Manuel Luna <manuellunada@gmail.com>
2022-07-22 19:37:39 -0500

    bubbles

--- .config/nvim/lua/plugins/config/lualine.lua
+++ .config/nvim/lua/plugins/config/lualine.lua
@@ -3,19 +3,19 @@ return {
 	config = function()
 		require('lualine').setup {
 			options = {
-				component_separators = '',
-				section_separators = { left = '', right = '' },
+				component_separators = '|',
+				section_separators = { left = '', right = '' },
 			},
 			sections = {
 				lualine_a = {
-					{ 'mode', separator = { left = '' }, right_padding = 2 },
+					{ 'mode', separator = { left = '' }, right_padding = 2 },
 				},
 				lualine_b = { 'filename', 'branch' },
 				lualine_c = { 'fileformat' },
 				lualine_x = {},
 				lualine_y = { 'filetype', 'progress' },
 				lualine_z = {
-					{ 'location', separator = { right = '' }, left_padding = 2 },
+					{ 'location', separator = { right = '' }, left_padding = 2 },
 				},
 			},
 			inactive_sections = {
