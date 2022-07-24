f008e3ad5ba722610b783916d8357b153307f3f5
Manuel Luna <manuellunada@gmail.com>
2022-07-24 14:33:32 -0500

    fix: truncate song title only when necessary

--- .local/scripts/osx/spotify.sh
+++ .local/scripts/osx/spotify.sh
@@ -11,8 +11,10 @@ title="$song_name - $artist"
 short_title=$(echo $title | cut -c -30)
 playing=$(grep -i paused <<< "$output" && echo no || echo yes)
 
+should_truncate=$([ ${#title} -gt 30 ] && echo yes || echo no)
+
 if [ "$playing" == "yes" ]; then
-	[ "$title" != "$short_title" ] && echo "#[fg=green]阮$short_title...#[fg=default]" || echo "#[fg=green]阮$title#[fg=default]"
+	[ "$should_truncate" == yes ] && echo "#[fg=green]阮$short_title...#[fg=default]" || echo "#[fg=green]阮$title#[fg=default]"
 else
-	[ "$title" != "$short_title" ] && echo "#[fg=gray]阮$short_title...#[fg=default]" || echo "#[fg=gray]阮$title#[fg=default]"
+	[ "$should_truncate" == yes ] && echo "#[fg=gray]阮$short_title...#[fg=default]" || echo "#[fg=gray]阮$title#[fg=default]"
 fi
