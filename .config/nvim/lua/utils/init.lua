16c405d27f2e64fde51464d66646ad08d0d10ffa
Manuel Luna <manuellunada@gmail.com>
2022-07-30 14:27:21 -0500

    set http files filetype

--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -3,5 +3,6 @@
 -- ╰──────────────────────────────────────────────────────────╯
 -- bruh
 vim.cmd([[
+autocmd! BufEnter *.http set ft=sh
 command Http :vert new | set ft=json | nmap <buffer> tt :bd! | read !sh #
 ]])
