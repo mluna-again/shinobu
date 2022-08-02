464e7b3b938607e7ed20fae0ee89d3c94164b45e
Manuel Luna <manuellunada@gmail.com>
2022-08-01 19:00:50 -0500

    keymap

--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -4,5 +4,5 @@
 -- bruh
 vim.cmd([[
 command Http :vert new | set ft=http | nmap <buffer> tt :bd! | read !sh #
-autocmd! BufEnter *.http set ft=sh | nmap <buffer> <C-h> :Http<CR>
+autocmd! BufEnter *.http set ft=sh | nmap <buffer> <Leader>h :Http<CR>
 ]])
