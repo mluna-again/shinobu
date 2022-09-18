ed82cf037ecb920d81646ba81c7be7c8eff91903
Manuel Luna <manuellunada@gmail.com>
2022-09-18 16:28:59 -0500

    fix elixirls

--- .config/nvim/lua/custom/plugins/lspconfig.lua
+++ .config/nvim/lua/custom/plugins/lspconfig.lua
@@ -2,11 +2,20 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "elixirls", "tsserver" }
+local servers = { "tsserver" }
+
+local home = os.getenv("HOME")
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
-    capabilities = capabilities,
+    capabilities = capabilities
   }
 end
+
+-- elixirls needs special treatment -_-
+lspconfig["elixirls"].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { string.format("%s/.local/share/nvim/mason/packages/elixir-ls/language_server.sh", home) }
+}
