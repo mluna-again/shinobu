32e1d78c8628c116d9a049b53239a1ac386ee5e0
Manuel Luna <manuellunada@gmail.com>
2022-09-18 16:22:37 -0500

    lsp

---
+++ .config/nvim/lua/custom/plugins/alpha.lua
@@ -0,0 +1,72 @@
+local function button(sc, txt, keybind)
+  local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
+
+  local opts = {
+    position = "center",
+    text = txt,
+    shortcut = sc,
+    cursor = 5,
+    width = 36,
+    align_shortcut = "right",
+    hl = "AlphaButtons",
+  }
+
+  if keybind then
+    opts.keymap = { "n", sc_, keybind, { noremap = true, silent = true } }
+  end
+
+  return {
+    type = "button",
+    val = txt,
+    on_press = function()
+      local key = vim.api.nvim_replace_termcodes(sc_, true, false, true) or ""
+      vim.api.nvim_feedkeys(key, "normal", false)
+    end,
+    opts = opts,
+  }
+end
+
+return {
+  headerPaddingTop = { type = "padding", val = 2 },
+  header = {
+    val = {
+      "┌────────────────────────────────────┐",
+      "│         ▂▄▅▇████████████▆▅▃        │",
+      "│      ▁▅▇███▝▘▘       ▝▝▝▘███▅▁     │",
+      "│     ▄██▘▘       ▝▘██       ▝██▖    │",
+      "│    ▇█▘       ╴               ▝█▆▅▖ │",
+      "│ ▗███▘▝▅▇▇▅▆▆▋╶    ▝▝▘▁        ▝██▇▍│",
+      "│ █▃█┙▗▆██████▉        ▘         ▚██▋│",
+      "│ █▂▉ ╍████████╴                 ▝█┻▉│",
+      "│▗█▚▊  ┻███▋╏╲▘                   █▆▘│",
+      "│  ▗▏    ▘▊╸                      ▍  │",
+      "│  ▝▎ ▁▃▁         ▃▃▄▃            ▍  │",
+      "│   ▊ ▝█▘       ▗█████▊          ╱▏  │",
+      "│   ▝▖         ▗███████▌        ╻    │",
+      "│    ▝▖       ▗█████████▆            │",
+      "│     █▆▃   ▂▃█████▌█████▇▇▄─▁▅▅▆    │",
+      "│ ▁╶▅▅██▇█▇▅███████▌███████▇█▇██▉ ▂  │",
+      "│██▆▁▌▝▝██▇████████▊████████▇▇█┛▘▂█▄▆│",
+      "│████▉▇▅▂▂▁▇███████▊████████▃▃▅▆████▊│",
+      "│▝████▖████████████▌███████████████▚▉│",
+      "│┣████▇████████████ ▝█████████████▋█▊│",
+      "│██▅█████████████▋    ▂█████████████▊│",
+      "│████████████████▇    ██████████████▉│",
+      "│████████████████▘    ▝█████████████▉│",
+      "│███████████████▘      ▝████████████▉│",
+      "│██████████▇██▘          ▝██████████▉│",
+      "│████████████▋            ▝█████████▉│",
+      "└────────────────────────────────────┘",
+    }
+  },
+  buttons = {
+    val = {
+      button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
+      button("SPC f o", "  Recent Files  ", ":Telescope oldfiles<CR>"),
+      button("SPC f w", "  Find Word  ", ":Telescope live_grep<CR>"),
+      button("SPC s l", "勒 Reload Last Session  ", ":source Session.vim<CR>"),
+      button("SPC t h", "  Themes  ", ":Telescope themes<CR>"),
+      button("q", "ﴘ Quit Neovim", ":exit<CR>"),
+    }
+  }
+}
---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,24 @@
+return {
+  ["folke/which-key.nvim"] = {
+    disable = false
+  },
+  ["goolord/alpha-nvim"] = {
+    disable = false,
+    override_options = require("custom.plugins.alpha")
+  },
+  ["numToStr/Comment.nvim"] = false,
+  ["tpope/vim-commentary"] = {},
+  ["neovim/nvim-lspconfig"] = {
+    config = function ()
+      require("plugins.configs.lspconfig")
+      require("custom.plugins.lspconfig")
+    end
+  },
+  ["williamboman/mason.nvim"] = {
+    override_options = {
+      ensure_installed = { "elixir-ls", "typescript-language-server" }
+    }
+  },
+  ["sbdchd/neoformat"] = {},
+  ["tpope/vim-obsession"] = {}
+}
