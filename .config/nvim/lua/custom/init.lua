19fe0351c0fcd14913240937695bfd7bbaf4b2ca
Manuel Luna <manuellunada@gmail.com>
2022-09-30 00:39:28 -0500

    fold/unfold

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -2,3 +2,14 @@ vim.cmd("set relativenumber")
 vim.cmd("command Close :bufdo bd | Alpha")
 vim.cmd("command CLose :bufdo bd | Alpha")
 vim.cmd("command CLOse :bufdo bd | Alpha")
+vim.cmd([[
+  set foldmethod=expr
+  set foldexpr=nvim_treesitter#foldexpr()
+  set foldtext=substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend))
+  set fillchars=fold:\\
+  set foldnestmax=3
+  set foldminlines=1
+  autocmd! BufReadPost,FileReadPost * normal zR
+  command! Fold :e | normal zMzr
+  command! Unfold normal zR
+]])
