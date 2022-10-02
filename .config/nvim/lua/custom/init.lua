018a0dc55f5ffd0ec2841c9e639a4c19c33b943d
Manuel Luna <manuellunada@gmail.com>
2022-10-02 14:40:47 -0500

    matchit

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -9,7 +9,9 @@ vim.cmd([[
   set fillchars=fold:\\
   set foldnestmax=3
   set foldminlines=1
-  autocmd! BufReadPost,FileReadPost * normal zR
+  autocmd! BufEnter * normal zR
   command! Fold :e | normal zMzr
   command! Unfold normal zR
 ]])
+
+vim.g.loaded_matchit = nil
