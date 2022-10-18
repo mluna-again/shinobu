c4d63ec216a6e5dd712d65cbca781fbcc1b03aa0
Manuel Luna <manuellunada@gmail.com>
2022-10-18 17:36:30 -0500

    nvim config

--- .config/nvim/lua/custom/plugins/lspconfig.lua
+++ .config/nvim/lua/custom/plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "tsserver", "solargraph", "cssls", "gopls", "kotlin_language_server" }
+local servers = { "tsserver", "solargraph", "cssls", "gopls", "kotlin_language_server", "ocamlls" }
 
 local home = os.getenv("HOME")
 
