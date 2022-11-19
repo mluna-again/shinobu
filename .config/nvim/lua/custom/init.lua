f43c2ccd64a2b6eb68c37877bbe3295bb5dea11c
Manuel Luna <manuellunada@gmail.com>
2022-11-18 19:31:14 -0600

    winsep

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -10,14 +10,3 @@ vim.cmd([[
   augroup END
 ]])
 vim.g.loaded_matchit = nil
--- vim.cmd([[
---   set foldmethod=expr
---   set foldexpr=nvim_treesitter#foldexpr()
---   set foldtext=substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend))
---   set fillchars=fold:\\
---   set foldnestmax=3
---   set foldminlines=1
---   autocmd! BufEnter * normal zR
---   command! Fold :e | normal zMzr
---   command! Unfold normal zR
--- ]])
