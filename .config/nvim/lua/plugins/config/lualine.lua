3cbc347c94271a15d8623d693055255067d0e328
Manuel Luna <manuellunada@gmail.com>
2022-11-27 22:11:31 -0600

    hide filetype in terminal

--- .config/nvim/lua/plugins/config/lualine.lua
+++ .config/nvim/lua/plugins/config/lualine.lua
@@ -1,6 +1,17 @@
 return {
 	'hoob3rt/lualine.nvim',
 	config = function()
+		local function shouldShowFilename()
+			local badFiletypes = { 'toggleterm', 'dashboard' }
+			local ft = vim.bo.filetype
+			for _, filetype in pairs(badFiletypes) do
+				if string.find(ft, filetype) then
+					return false
+				end
+			end
+			return true
+		end
+
 		require('lualine').setup {
 			options = {
 				component_separators = '',
@@ -14,22 +25,13 @@ return {
 				lualine_a = {
 					{ 'mode', separator = { left = '' }, right_padding = 2, icon = '', color = { gui = 'bold' } },
 				},
-				lualine_b = { { 'filetype', icon_only = true }, 'filename' },
-				lualine_c = { { 'branch',  icon = '' } },
-				lualine_x = { 'diagnostics' },
-				lualine_y = { { 'vim.fn.fnamemodify(vim.fn.getcwd(), ":t")', icon = ' ' } },
-				lualine_z = { { 'progress', icon = '' } },
+				lualine_b = { { 'filetype', icon_only = true, cond = shouldShowFilename }, { 'filename', cond = shouldShowFilename }
 			},
-			inactive_sections = {
-				lualine_a = { 'filename' },
-				lualine_b = {},
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = { 'location' },
-			},
-			tabline = {},
-			extensions = {},
+			lualine_c = { { 'branch',  icon = '' } },
+			lualine_x = { 'diagnostics' },
+			lualine_y = { { 'vim.fn.fnamemodify(vim.fn.getcwd(), ":t")', icon = ' ' } },
+			lualine_z = { { 'progress', icon = '' } },
 		}
-	end
+	}
+end
 }
