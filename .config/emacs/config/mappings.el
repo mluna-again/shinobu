e03f9cc7c77ff89af540efbca1acf84e8146e07d
Manuel Luna <manuellunada@gmail.com>
2022-11-29 21:55:00 -0600

    helm mappings

--- .config/emacs/config/mappings.el
+++ .config/emacs/config/mappings.el
@@ -9,8 +9,8 @@
 (define-key evil-normal-state-map (kbd "ss") 'save-buffer)
 
 ;; Helm
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(define-key evil-normal-state-map (kbd "SPC ff") 'helm-find-files)
+(global-set-key (kbd "C-x C-f") 'helm-find)
+(define-key evil-normal-state-map (kbd "SPC ff") 'helm-find)
 (define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
 (define-key helm-map (kbd "<tab>") #'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-z") #'helm-select-action)
