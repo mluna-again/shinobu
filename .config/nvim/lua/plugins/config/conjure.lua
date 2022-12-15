a37974cf27c3e1f3defacb60cb3c1a2a381ce801
Manuel Luna <manuellunada@gmail.com>
2022-12-14 20:04:49 -0600

    add conjure again...

--- .config/nvim/lua/plugins/config/conjure.lua
+++ .config/nvim/lua/plugins/config/conjure.lua
@@ -6,5 +6,9 @@ return {
 		nmap("<leader>cl", ":ConjureEvalRootForm<CR>")
 		nmap("<leader>ce", ":ConjureEvalCurrentForm<CR>")
 		nmap("<leader>cp", ":ConjureEvalBuf<CR>")
+
+		vim.cmd([[
+		autocmd BufNewFile conjure-log-* lua vim.diagnostic.disable(0)
+		]])
 	end
 }
