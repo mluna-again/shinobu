e312f8939d4ab25177ca2f363193c720f377febe
Manuel Luna <manuellunada@gmail.com>
2022-12-19 23:39:52 -0600

    java is no more

--- .config/nvim/lua/plugins/config/nvim-cmp.lua
+++ .config/nvim/lua/plugins/config/nvim-cmp.lua
@@ -129,9 +129,6 @@ return {
 		require('lspconfig').clojure_lsp.setup{
 			capabilities = capabilities
 		}
-		require('lspconfig').jdtls.setup{
-			capabilities = capabilities
-		}
 
 		nmap('<Leader>lh', ':lua vim.lsp.buf.hover()<CR>')
 		nmap('<Leader>lr', ':lua vim.lsp.buf.rename()<CR>')
