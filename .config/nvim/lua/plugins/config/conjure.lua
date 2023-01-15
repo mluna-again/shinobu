7a14d2e0ba0b436701c5cdbce42dfc4931975acd
Manuel Luna <manuellunada@gmail.com>
2023-01-15 13:45:50 -0600

    don't auto start conjure

--- .config/nvim/lua/plugins/config/conjure.lua
+++ .config/nvim/lua/plugins/config/conjure.lua
@@ -2,12 +2,14 @@ require("core.maps")
 
 return {
 	'Olical/conjure',
+	commit = 'd2e69a13b32e8574decfe81ea275292234eba6ea',
 	config = function ()
 		nmap("<leader>cl", ":ConjureEvalRootForm<CR>")
 		nmap("<leader>ce", ":ConjureEvalCurrentForm<CR>")
 		nmap("<leader>cp", ":ConjureEvalBuf<CR>")
 
 		vim.cmd([[
+		let g:conjure#client_on_load = v:false
 		autocmd BufNewFile conjure-log-* lua vim.diagnostic.disable(0)
 		]])
 	end
