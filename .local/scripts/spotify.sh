9aad223effe7f206e3cfc0411a9fba495314b1df
Manuel Luna <manuellunada@gmail.com>
2023-01-27 20:07:03 -0600

    bruh

--- .local/scripts/spotify.sh
+++ .local/scripts/spotify.sh
@@ -1,11 +1,7 @@
 #! /bin/sh
 
-truncate() {
-  printf "%.45s" "$1"
-}
-
 if uname | grep -i darwin &>/dev/null; then
-  truncate "$(~/.local/scripts/osx/spotify.sh)"
+  ~/.local/scripts/osx/spotify.sh
 else
-  truncate "$(~/.local/scripts/linux/spotify.sh)"
+	~/.local/scripts/linux/spotify.sh
 fi
