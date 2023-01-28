99e1058829e23a5f87cb35f5e8dad6f28aa54590
Manuel Luna <manuellunada@gmail.com>
2023-01-27 20:07:03 -0600

    bruh

--- .local/scripts/osx/spotify.sh
+++ .local/scripts/osx/spotify.sh
@@ -5,13 +5,13 @@ command -v spotify &>/dev/null || { echo "No spotify!"; exit; }
 pgrep Spotify &>/dev/null || exit
 
 output=$(spotify status)
-song_name=$(grep -i track <<< "$output" | sed 's/Track: //')
-artist=$(grep -i artist <<< "$output" | sed 's/Artist: //')
+song_name=$(grep -i track: <<< "$output" | sed 's/Track: //')
+artist=$(grep -i artist: <<< "$output" | sed 's/Artist: //')
 title="$song_name- $artist"
-short_title=$(echo $title | cut -c -45)
+short_title=$(echo $title | cut -c -25)
 playing=$(grep -i paused <<< "$output" && echo no || echo yes)
 
-should_truncate=$([ ${#title} -gt 45 ] && echo yes || echo no)
+should_truncate=$([ ${#title} -gt 25 ] && echo yes || echo no)
 
 if [ "$playing" == "yes" ]; then
 	[ "$should_truncate" == yes ] && echo "#[fg=green]阮 $short_title...#[fg=default]" || echo "#[fg=green]阮 $title#[fg=default]"
