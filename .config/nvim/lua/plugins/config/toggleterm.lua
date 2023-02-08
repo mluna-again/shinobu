2161e11275397cb716ff68bb62d566929983b0b0
Manuel Luna <manuellunada@gmail.com>
2023-02-08 12:30:07 -0600

    terminal height

--- .config/nvim/lua/plugins/config/toggleterm.lua
+++ .config/nvim/lua/plugins/config/toggleterm.lua
@@ -7,7 +7,7 @@ return {
 			-- size can be a number or function which is passed the current terminal
 			size = function(term)
 				if term.direction == "horizontal" then
-					return vim.api.nvim_buf_line_count(0) * 0.4
+					return 15
 				elseif term.direction == "vertical" then
 					return vim.o.columns * 0.5
 				end
