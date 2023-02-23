377329e92665694830325aca8dd2e2788b05a7e5
Manuel Luna <manuellunada@gmail.com>
2023-02-22 21:14:51 -0600

    colors

--- .local/scripts/osx/battery.sh
+++ .local/scripts/osx/battery.sh
@@ -6,21 +6,21 @@ low_battery=$([ $percentage -le 20 ] && echo yes || echo no)
 plugged=$(system_profiler SPPowerDataType | grep -i "connected: no" && echo no || echo yes)
 
 if [ "$plugged" == "yes" ]; then
-	echo "#[fg=green]  $percentage%"
+	echo "#[fg=#252535,bg=#76946A]   $percentage%"
 elif [ "$low_battery" == "no" ]; then
 
 	if [ "$percentage" -ge "90" ]; then
-		echo "#[fg=green] $percentage%"
+		echo "#[fg=#252535,bg=#76946A]  $percentage%"
 	elif [ "$percentage" -ge "70" ]; then
-		echo "#[fg=green] $percentage%"
+		echo "#[fg=#252535,bg=#76946A]  $percentage%"
 	elif [ "$percentage" -ge "50" ]; then
-		echo "#[fg=yellow] $percentage%"
+		echo "#[fg=#252535,bg=#E6C384]  $percentage%"
 	elif [ "$percentage" -ge "30" ]; then
-		echo "#[fg=yellow] $percentage%"
+		echo "#[fg=#252535,bg=#E6C384]  $percentage%"
 	elif [ "$percentage" -ge "20" ]; then
-		echo "#[fg=orange] $percentage%"
+		echo "#[fg=#252535,bg=#E46876]  $percentage%"
 	fi
 
 else
-	echo "#[fg=red] $percentage%"
+	echo "#[fg=#252535,bg=#E46876]  $percentage%"
 fi
