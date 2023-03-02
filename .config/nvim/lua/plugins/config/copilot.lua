fa1f8f80b8fb8c85a2c1333cebc512ddf23ab38d
Manuel Luna <manuellunada@gmail.com>
2023-03-02 15:28:09 -0600

    copilot file

---
+++ .config/nvim/lua/plugins/config/copilot.lua
@@ -0,0 +1,12 @@
+require("core.maps")
+
+return {
+	'github/copilot.vim',
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.g.copilot_filetypes = {
+			text = false
+		}
+		imap("<C-J>", "copilot#Accept(\"<CR>\")", {silent = true, expr = true, script = true})
+	end
+}
---
+++ .config/nvim/lua/plugins/config/table-mode.lua
@@ -0,0 +1,5 @@
+return {
+	'dhruvasagar/vim-table-mode',
+	config = function()
+	end
+}
