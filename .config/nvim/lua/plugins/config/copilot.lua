42697e00d77b128eabbc9f48096f3ec3ea0c48fb
Manuel Luna <manuellunada@gmail.com>
2023-03-20 17:09:31 -0600

    disable copilot by default

--- .config/nvim/lua/plugins/config/copilot.lua
+++ .config/nvim/lua/plugins/config/copilot.lua
@@ -3,6 +3,7 @@ require("core.maps")
 return {
 	'github/copilot.vim',
 	config = function()
+		vim.g.copilot_enabled = false
 		vim.g.copilot_no_tab_map = true
 		vim.g.copilot_filetypes = {
 			text = false
