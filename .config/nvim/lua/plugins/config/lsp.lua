0c28944a7a6fa577edc459c01c06d0b5579fde63
Manuel Luna <manuellunada@gmail.com>
2023-05-19 15:00:14 -0600

    go linter

--- .config/nvim/lua/plugins/config/lsp.lua
+++ .config/nvim/lua/plugins/config/lsp.lua
@@ -1,34 +1,35 @@
 return {
-	'williamboman/mason-lspconfig.nvim',
+	"williamboman/mason-lspconfig.nvim",
 	config = function()
-		require('mason-lspconfig').setup()
+		require("mason-lspconfig").setup()
 
-		local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+		local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 		local servers = {
-			'elixirls',
-			'tsserver',
-			'cssls',
-			'solargraph',
-			'rust_analyzer',
-			'clojure_lsp',
-			'kotlin_language_server',
-			'gopls',
-			'metals',
-			'omnisharp'
+			"elixirls",
+			"tsserver",
+			"cssls",
+			"solargraph",
+			"rust_analyzer",
+			"clojure_lsp",
+			"kotlin_language_server",
+			"gopls",
+			"metals",
+			"omnisharp",
+			"golangci_lint_ls",
 		}
 
 		for _, server in pairs(servers) do
-			local s = require('lspconfig')[server]
-			require('lspconfig')[server].setup{
-				capabilities = capabilities
-			}
+			local s = require("lspconfig")[server]
+			require("lspconfig")[server].setup({
+				capabilities = capabilities,
+			})
 		end
 
-		nmap('<Leader>lh', ':lua vim.lsp.buf.hover()<CR>')
-		nmap('<Leader>lr', ':lua vim.lsp.buf.rename()<CR>')
-		nmap('<Leader>lf', ':lua vim.lsp.buf.definition()<CR>')
-		nmap('<Leader>ld', ':lua vim.diagnostic.open_float()<CR>')
-		nmap('<Leader>ll', ':LspRestart<CR>')
-	end
+		nmap("<Leader>lh", ":lua vim.lsp.buf.hover()<CR>")
+		nmap("<Leader>lr", ":lua vim.lsp.buf.rename()<CR>")
+		nmap("<Leader>lf", ":lua vim.lsp.buf.definition()<CR>")
+		nmap("<Leader>ld", ":lua vim.diagnostic.open_float()<CR>")
+		nmap("<Leader>ll", ":LspRestart<CR>")
+	end,
 }
