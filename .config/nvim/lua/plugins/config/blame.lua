9ff88a82da21c5771f42ae662e883b85cd9bee43
Manuel Luna <manuellunada@gmail.com>
2023-05-28 22:49:13 -0600

    missing file

---
+++ .config/nvim/lua/plugins/config/blame.lua
@@ -0,0 +1,10 @@
+return {
+  "f-person/git-blame.nvim",
+  config = function ()
+    vim.cmd([[
+    let g:gitblame_delay = 1000
+    let g:gitblame_message_template = '#<sha> -> "<summary>" by <author> • <date>'
+    let g:gitblame_date_format = '%r'
+    ]])
+  end
+}
