6954aeedec5491c322abd00469c6e4c5df021ca6
Manuel Luna <manuellunada@gmail.com>
2023-05-29 00:57:05 -0600

    use default bufferline

--- .config/nvim/lua/plugins/config/bufferline.lua
+++ .config/nvim/lua/plugins/config/bufferline.lua
@@ -20,66 +20,7 @@ return {
 				always_show_bufferline = false,
 				diagnostics = false
 			},
-			highlights = {
-				indicator_selected = {
-					bg = "#16161D",
-					fg = "#16161D"
-				},
-				modified_selected = {
-					bg = "#16161D",
-					fg = "#FFA066"
-				},
-				modified_visible = {
-					bg = "#16161D",
-					fg = "#FFA066"
-				},
-				modified = {
-					bg = "#16161D",
-					fg = "#FFA066"
-				},
-				pick = {
-					bg = "#16161D",
-					fg = "#16161D"
-				},
-				pick_visible = {
-					bg = "#16161D",
-					fg = "#16161D"
-				},
-				pick_selected = {
-					bg = "#16161D",
-					fg = "#16161D"
-				},
-				separator = {
-					bg = "#16161D",
-					fg = "#16161D"
-				},
-				separator_visible = {
-					bg = "#16161D",
-					fg = "#16161D"
-				},
-				separator_selected = {
-					bg = "#16161D",
-					fg = "#16161D"
-				},
-				fill = {
-					bg = "#16161D"
-				},
-				background = {
-					bg = "#16161D"
-				},
-				buffer = {
-					fg = "#DCD7BA",
-					bg = "#16161D"
-				},
-				buffer_selected = {
-					fg = "#DCD7BA",
-					bg = "#16161D"
-				},
-				buffer_visible = {
-					fg = "#717C7C",
-					bg = "#16161D"
-				}
-			}
+			highlights = {}
 		}
 	end
 }
