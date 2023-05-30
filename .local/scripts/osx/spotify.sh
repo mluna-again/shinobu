fbff8611e6e6a141724bb8cd32b2c01a2e089273
Manuel Luna <manuellunada@gmail.com>
2023-05-30 12:46:35 -0600

    fallback

--- .local/scripts/osx/spotify.sh
+++ .local/scripts/osx/spotify.sh
@@ -1,21 +1,31 @@
 #! /bin/sh
 
-[[ -S ~/.cache/ncspot/ncspot.sock ]] || exit
-pgrep ncspot || exit
+pgrep ncspot &>/dev/null || pgrep Spotify &>/dev/null || exit
 
-output=$(nc -w 1 -U ~/.cache/ncspot/ncspot.sock)
-song_name=$(echo $output | jq '.playable.title' | sed 's/"//g')
-artist=$(echo $output | jq '.playable.artists[0]' | sed 's/"//g')
+using_spotify=$(pgrep ncspot &>/dev/null && echo no || echo yes)
+get_info() {
+	[ $using_spotify = no ] && { nc -w 1 -U ~/.cache/ncspot/ncspot.sock && return; }
+}
+
+output=$(get_info)
+
+get_song_name() {
+	[ $using_spotify = no ] && { echo $output | jq '.playable.title' | sed 's/"//g' && return; }
+
+	spotify status track
+}
+song_name=$(get_song_name)
+
+get_artist() {
+	[ $using_spotify = no ] && { echo $output | jq '.playable.artists[0]' | sed 's/"//g' && return; }
+
+	spotify status artist
+}
+artist=$(get_artist)
+
 title="$song_name - $artist"
 short_title=$(echo $title | cut -c -30)
-playing=$(grep -i paused <<< "$output" && echo no || echo yes)
-
-[ "$playing" == "yes" ] || exit
 
 should_truncate=$([ ${#title} -gt 30 ] && echo yes || echo no)
 
-if [ "$playing" == "yes" ]; then
-	[ "$should_truncate" == yes ] && echo "#[bg=red,fg=black] 阮 $short_title...#[fg=default]" || echo "#[bg=red,fg=black] 阮 $title#[fg=default]"
-else
-	[ "$should_truncate" == yes ] && echo "#[bg=black,fg=red] 阮 $short_title...#[fg=default]" || echo "#[bg=black,fg=red] 阮 $title#[fg=default]"
-fi
+[ "$should_truncate" == yes ] && echo "#[bg=red,fg=black] 阮 $short_title...#[fg=default]" || echo "#[bg=red,fg=black] 阮 $title#[fg=default]"
