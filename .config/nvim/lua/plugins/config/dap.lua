345136718ae68891fe748795d9b2c6013f005a58
Manuel Luna <manuellunada@gmail.com>
2023-06-02 20:16:38 -0600

    stop debugger mapping

--- .config/nvim/lua/plugins/config/dap.lua
+++ .config/nvim/lua/plugins/config/dap.lua
@@ -5,24 +5,32 @@ return {
 		require("dapui").setup()
 		require("dap-go").setup()
 
-		-- auto open/close dapui
 		local dap, dapui = require("dap"), require("dapui")
-		dap.listeners.after.event_initialized["dapui_config"] = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated["dapui_config"] = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited["dapui_config"] = function()
-			dapui.close()
-		end
-
 		local wk = require("which-key")
 		-- mappings
 		wk.register({
 			d = {
+				name = "Debugging",
 				b = { ":lua require'dap'.toggle_breakpoint()<CR>", "Toggle breakpoint", noremap = true, silent = true },
-				c = { ":lua require'dap'.continue()<CR>", "Continue/Start debugging", noremap = true, silent = true },
+				B = { ":lua require'dap'.clear_breakpoints()<CR>", "Clear breakpoints", noremap = true, silent = true },
+				c = {
+					function()
+						dapui.open({ reset = true })
+						dap.continue()
+					end,
+					"Continue/Start debugging",
+					noremap = true,
+					silent = true,
+				},
+				x = {
+					function()
+						dap.close()
+						dapui.close()
+					end,
+					"Stop debugging",
+					noremap = true,
+					silent = true,
+				},
 				n = { ":lua require'dap'.step_over()<CR>", "Step over", noremap = true, silent = true },
 				N = { ":lua require'dap'.step_into()<CR>", "Step into", noremap = true, silent = true },
 				r = { ":lua require'dap'.repl()<CR>", "REPL", noremap = true, silent = true },
