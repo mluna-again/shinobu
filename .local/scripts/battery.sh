4e314d947d77b1db65cafb39ef8a424749842c9b
Manuel Luna <manuellunada@gmail.com>
2023-06-15 18:16:11 -0600

    order

--- .local/scripts/battery.sh
+++ .local/scripts/battery.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if uname | grep -i darwin &>/dev/null; then
-	~/.local/scripts/osx/battery.sh
+	~/.local/scripts/osx/battery.sh "$@"
 else
-	~/.local/scripts/linux/battery.sh
+	~/.local/scripts/linux/battery.sh "$@"
 fi
--- .local/scripts/osx/battery.sh
+++ .local/scripts/osx/battery.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
+bg="red"
+fg="black"
+
 percentage=$(system_profiler SPPowerDataType | grep -i "state of charge" | awk '{print $(NF)}')
 
 low_battery=$([ $percentage -le 20 ] && echo yes || echo no)
 plugged=$(system_profiler SPPowerDataType | grep -i "connected: no" && echo no || echo yes)
 
 if [ "$plugged" == "yes" ]; then
-	echo "#[fg=black,bg=red]   $percentage%"
+	echo "#[fg=$fg,bg=$bg]   $percentage%"
 elif [ "$low_battery" == "no" ]; then
 
 	if [ "$percentage" -ge "90" ]; then
-		echo "#[fg=black,bg=red] 󰁹 $percentage%"
+		echo "#[fg=$fg,bg=$bg] 󰁹 $percentage%"
 	elif [ "$percentage" -ge "70" ]; then
-		echo "#[fg=black,bg=red] 󰂂 $percentage%"
+		echo "#[fg=$fg,bg=$bg] 󰂂 $percentage%"
 	elif [ "$percentage" -ge "50" ]; then
-		echo "#[fg=black,bg=red] 󰂀 $percentage%"
+		echo "#[fg=$fg,bg=$bg] 󰂀 $percentage%"
 	elif [ "$percentage" -ge "30" ]; then
-		echo "#[fg=black,bg=red] 󰁾 $percentage%"
+		echo "#[fg=$fg,bg=$bg] 󰁾 $percentage%"
 	elif [ "$percentage" -ge "20" ]; then
-		echo "#[fg=black,bg=red] 󰁻 $percentage%"
+		echo "#[fg=$fg,bg=$bg] 󰁻 $percentage%"
 	fi
 
 else
-	echo "#[fg=black,bg=red] 󰁺 $percentage%"
+	echo "#[fg=$fg,bg=$bg] 󰁺 $percentage%"
 fi
