1911724591e7ab297b79c8ef1ca59b53b929f1a3
Manuel Luna <manuellunada@gmail.com>
2023-06-26 16:32:07 -0600

    fix tmux path

--- .local/scripts/shortname/shortname.sh
+++ .local/scripts/shortname/shortname.sh
@@ -2,12 +2,12 @@
 
 osx() {
 	[ ! -e ./shortname_osx ] && go build -o shortname_osx &>/dev/null
-	./shortname_osx "$@"
+	~/.local/scripts/shortname/shortname_osx "$@"
 }
 
 linux() {
 	[ ! -e ./shortname_linux ] && go build -o shortname_linux &>/dev/null
-	./shortname_linux "$@"
+	~/.local/scripts/shortname/shortname_linux "$@"
 }
 
 if [ ! -z $(uname | grep -i darwin) ]; then
