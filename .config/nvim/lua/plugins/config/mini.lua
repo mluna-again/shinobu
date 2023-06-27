e2eb41f01db94e2245a78ab1ebc0fe33a637e992
Manuel Luna <manuellunada@gmail.com>
2023-06-27 12:18:35 -0600

    verylazy

--- .config/nvim/lua/plugins/config/mini.lua
+++ .config/nvim/lua/plugins/config/mini.lua
@@ -1,6 +1,7 @@
 return {
 	"echasnovski/mini.nvim",
 	version = false,
+	event = "VeryLazy",
 	config = function ()
 		require("mini.trailspace").setup()
 		vim.api.nvim_create_autocmd("BufWritePre", {
