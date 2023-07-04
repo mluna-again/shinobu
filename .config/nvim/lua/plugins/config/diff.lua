e12b8f22a3205a7e438a7d628bcd6b2652540d42
Manuel Luna <manuellunada@gmail.com>
2023-07-04 14:20:44 -0600

    diff config

--- .config/nvim/lua/plugins/config/diff.lua
+++ .config/nvim/lua/plugins/config/diff.lua
@@ -1,9 +1,23 @@
 return {
 	"sindrets/diffview.nvim",
 	event = "VeryLazy",
+	dependencies = {
+		"levouh/tint.nvim",
+	},
 	config = function()
 		vim.opt.fillchars:append { diff = "╱" }
 		require("diffview").setup({
+			hooks = {
+				diff_buf_read = function()
+					vim.opt_local.foldenable = false
+				end,
+				view_enter = function ()
+					require("tint").disable()
+				end,
+				view_leave = function ()
+					require("tint").enable()
+				end,
+			},
 			view = {
 				merge_tool = {
 					layout = "diff3_mixed",
