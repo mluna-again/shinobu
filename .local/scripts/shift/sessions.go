994d4a6df8f4e0fded6574eda61b8ab9db111e5a
Manuel Luna <manuellunada@gmail.com>
2023-07-08 23:09:20 -0600

    better filter algo (hopefully)

--- .local/scripts/shift/sessions.go
+++ .local/scripts/shift/sessions.go
@@ -6,6 +6,35 @@ import (
 	"github.com/charmbracelet/bubbles/table"
 )
 
+func hasAllLetters(session, query string) bool {
+	session = strings.TrimSpace(session)
+	seen := map[rune]int{}
+
+	for _, letter := range query {
+		if _, ok := seen[letter]; ok {
+			seen[letter]++
+			continue
+		}
+
+		seen[letter] = 1
+	}
+
+	for _, letter := range session {
+		if _, ok := seen[letter]; ok {
+			seen[letter]--
+			continue
+		}
+	}
+
+	for _, value := range seen {
+		if value > 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
 func (m *model) fuzzyFind() {
 	if len(m.input.Value()) == 0 {
 		m.filtered = m.app.lines
@@ -15,7 +44,7 @@ func (m *model) fuzzyFind() {
 	m.filtered = []string{}
 
 	for _, session := range m.app.lines {
-		if session != "" && strings.Contains(strings.TrimSpace(session), strings.TrimSpace(m.input.Value())) {
+		if session != "" && hasAllLetters(session, m.input.Value()) {
 			m.filtered = append(m.filtered, session)
 		}
 	}
