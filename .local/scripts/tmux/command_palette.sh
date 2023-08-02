94e52cf760d3d77661b3447db6e8a79f8a0efbe6
Manuel Luna <manuellunada@gmail.com>
2023-08-01 18:03:49 -0600

    prevent unnecessary popup

--- .local/scripts/tmux/command_palette.sh
+++ .local/scripts/tmux/command_palette.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 RESULTS_FILE="$HOME/.cache/.shift_command_result"
+CACHE_PATH="$HOME/.cache/.i_dont_know_how_to_program_and_my_code_should_be_illegal"
 
 commands="$(cat - <<EOF
 Notes
@@ -11,7 +12,8 @@ tmux display-popup -w 65 -h 11 -y 15 -E "rm \"$RESULTS_FILE\" ; $HOME/.local/scr
 
 case "$(tail -1 "$RESULTS_FILE")" in
 	Notes)
-		tmux display-popup -w "65" -h "11" -y 15 -E "$HOME/.local/scripts/notes/notes.sh" \; \
-			display-popup -b heavy -S fg=yellow -w "80%" -h "80%" -E "$HOME/.local/scripts/tmux/selectedcmd.sh"
+		tmux display-popup -w "65" -h "11" -y 15 -E "$HOME/.local/scripts/notes/notes.sh"
+		[ ! -e "$CACHE_PATH" ] && exit # no note selected
+		tmux display-popup -b heavy -S fg=yellow -w "80%" -h "80%" -E "$HOME/.local/scripts/tmux/selectedcmd.sh"
 		;;
 esac
