567076331024673d78f9f1dffb47169c4d771f6c
Manuel Luna <manuellunada@gmail.com>
2023-08-04 22:06:40 -0600

    catppuccin tweaks

--- .config/nvim/lua/plugins/config/catppuccin.lua
+++ .config/nvim/lua/plugins/config/catppuccin.lua
@@ -5,6 +5,13 @@ return {
 	config = function()
 		require("catppuccin").setup({
 			flavour = "mocha",
+			integrations = {
+				noice = true,
+				telescope = {
+					enabled = true,
+					style = "nvchad",
+				},
+			},
 		})
 	end,
 }
