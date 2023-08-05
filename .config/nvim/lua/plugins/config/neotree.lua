f56b58d570aebd7f802613d2b48186ad6efb6975
Manuel Luna <manuellunada@gmail.com>
2023-08-04 19:42:41 -0600

    fix broken icons

--- .config/nvim/lua/plugins/config/neotree.lua
+++ .config/nvim/lua/plugins/config/neotree.lua
@@ -23,7 +23,7 @@ return {
 		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
 		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
 		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-		vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+		vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
 
 		require("neo-tree").setup({
 			source_selector = {
--- .config/nvim/lua/plugins/config/todo.lua
+++ .config/nvim/lua/plugins/config/todo.lua
@@ -16,8 +16,8 @@ return {
 				TODO = { icon = " ", color = "info" },
 				HACK = { icon = " ", color = "warning" },
 				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-				PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+				PERF = { icon = "󰓅 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
 			},
 			merge_keywords = true, -- when true, custom keywords will be merged with the defaults
 			-- highlighting of the line containing the todo comment
