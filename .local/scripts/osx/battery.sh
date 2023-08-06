5bd058bd51f4814a1ac0f67272d1e65ee252ebea
Manuel Luna <manuellunada@gmail.com>
2023-08-06 01:45:50 -0600

    tmux icons

--- .local/scripts/osx/battery.sh
+++ .local/scripts/osx/battery.sh
@@ -6,21 +6,21 @@ low_battery=$([ $percentage -le 20 ] && echo yes || echo no)
 plugged=$(system_profiler SPPowerDataType | grep -i "connected: no" && echo no || echo yes)
 
 if [ "$plugged" == "yes" ]; then
-	echo "   $percentage%"
+	echo "   #[bg=terminal,fg=terminal] $percentage%"
 elif [ "$low_battery" == "no" ]; then
 
 	if [ "$percentage" -ge "90" ]; then
-		echo " 󰁹 $percentage%"
+		echo " 󰁹 #[bg=terminal,fg=terminal] $percentage%"
 	elif [ "$percentage" -ge "70" ]; then
-		echo " 󰂂 $percentage%"
+		echo " 󰂂 #[bg=terminal,fg=terminal] $percentage%"
 	elif [ "$percentage" -ge "50" ]; then
-		echo " 󰂀 $percentage%"
+		echo " 󰂀 #[bg=terminal,fg=terminal] $percentage%"
 	elif [ "$percentage" -ge "30" ]; then
-		echo " 󰁾 $percentage%"
+		echo " 󰁾 #[bg=terminal,fg=terminal] $percentage%"
 	elif [ "$percentage" -ge "20" ]; then
-		echo " 󰁻 $percentage%"
+		echo " 󰁻 #[bg=terminal,fg=terminal] $percentage%"
 	fi
 
 else
-	echo " 󰁺 $percentage%"
+	echo " 󰁺 #[bg=terminal,fg=terminal] $percentage%"
 fi
