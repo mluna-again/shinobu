ca1ce88a53589a4deba0cbd76cc417a59407924f
Manuel Luna <manuellunada@gmail.com>
2023-08-07 20:25:38 -0600

    fix linux battery script

--- .local/scripts/linux/battery.sh
+++ .local/scripts/linux/battery.sh
@@ -7,7 +7,7 @@ battery=$(upower -e 2>/dev/null | grep -i bat0)
 
 # something bad happened, for example, you tried to use wsl (windows 🤮) and
 # that is something very, very bad.
-[ -z "$battery" ] && { echo " 󰂑 #[bg=terminal,fg=terminal] ???%"; exit; }
+[ -z "$battery" ] && { echo "???% #[bg=$2,fg=black] 󰂑 "; exit; }
 
 percentage=$(upower -i "$battery" | grep -i percentage | awk '{print $2}')
 percentage=$(echo "$percentage" | cut -c -3)
@@ -16,22 +16,22 @@ low_battery=$([ "$percentage" -le 20 ] && echo "yes" || echo "no")
 
 plugged=$(upower -i "$battery" | grep -i ".*state:.*\bcharging\b.*" &>/dev/null && echo "yes" || echo "no")
 
-if [ "$plugged" == "yes" ]; then
-	echo "   #[bg=terminal,fg=terminal] $percentage%"
-elif [ "$low_battery" == "no" ]; then
+if [ "$plugged" = "yes" ]; then
+	echo "#[bg=terminal,fg=terminal]$percentage% #[bg=$2,fg=black]  "
+elif [ "$low_battery" = "no" ]; then
 
 	if [ "$percentage" -ge "90" ]; then
-		echo " 󰁹 #[bg=terminal,fg=terminal] $percentage%"
+		echo "#[bg=terminal,fg=terminal]$percentage% #[bg=$2,fg=black] 󰁹 "
 	elif [ "$percentage" -ge "70" ]; then
-		echo " 󰂂 #[bg=terminal,fg=terminal] $percentage%"
+		echo "#[bg=terminal,fg=terminal]$percentage% #[bg=$2,fg=black] 󰂂 "
 	elif [ "$percentage" -ge "50" ]; then
-		echo " 󰂀 #[bg=terminal,fg=terminal] $percentage%"
+		echo "#[bg=terminal,fg=terminal]$percentage% #[bg=$2,fg=black] 󰂀 "
 	elif [ "$percentage" -ge "30" ]; then
-		echo " 󰁾 #[bg=terminal,fg=terminal] $percentage%"
+		echo "#[bg=terminal,fg=terminal]$percentage% #[bg=$2,fg=black] 󰁾 "
 	elif [ "$percentage" -ge "20" ]; then
-		echo " 󰁻 #[bg=terminal,fg=terminal] $percentage%"
+		echo "#[bg=terminal,fg=terminal]$percentage% #[bg=$2,fg=black] 󰁻 "
 	fi
 
 else
-	echo " 󰁺 #[bg=terminal,fg=terminal] $percentage%"
+	echo "#[bg=terminal,fg=terminal]$percentage% #[bg=$2,fg=black] 󰁺 "
 fi
