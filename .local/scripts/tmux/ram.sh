c5dbe90eb230b53c517d50e889bb5d76501778b3
Manuel Luna <manuellunada@gmail.com>
2023-09-29 01:58:06 -0600

    fix linux ram script

--- .local/scripts/tmux/ram.sh
+++ .local/scripts/tmux/ram.sh
@@ -3,5 +3,5 @@
 if uname | grep -i darwin &>/dev/null; then
 	top -l 1 | grep -E "^PhysMem"  | awk '{print $2}' | numfmt --to=iec --from=si
 else
-	free | awk 'NR == 2 {print $3}' | numfmt --to=iec --from=si
+	free -b | awk 'NR == 2 {print $3}' | numfmt --to=iec --from=si
 fi
