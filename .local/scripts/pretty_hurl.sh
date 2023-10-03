12d546504a0fdc74123c5fe159a99b432217ae7d
Manuel Luna <manuellunada@gmail.com>
2023-10-02 20:05:24 -0600

    jq query

--- .local/scripts/pretty_hurl.sh
+++ .local/scripts/pretty_hurl.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
 
-output=$(hurl --color -iL "$@")
+file="$1"
+query="${2:-.}"
+
+output=$(hurl --color -iL "$file")
 headers=$(awk '{ if (NF == 0) over = 1 } { if (over == 0) { print $0 } }' <<< "$output")
 body=$(awk '{ if (NF == 0) over = 1 } { if (over > 0) { print $0 } }' <<< "$output")
 
 printf "%s\n\n" "$headers"
-jq '.' <<< "$body" 2>/dev/null || printf "%s\n" "$body"
+jq "$query" <<< "$body" 2>/dev/null || printf "%s\n" "$body"
