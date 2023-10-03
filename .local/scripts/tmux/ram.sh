137a9bd3de93cb9953bfc349071e15378b76fad3
Manuel Luna <manuellunada@gmail.com>
2023-10-02 19:10:17 -0600

    dynamic ram module

--- .local/scripts/tmux/ram.sh
+++ .local/scripts/tmux/ram.sh
@@ -1,7 +1,14 @@
 #! /usr/bin/env bash
 
+background="$1"
+width="$2"
+
+[ "$width" -lt 100 ] && exit
+
 if uname | grep -i darwin &>/dev/null; then
-	top -l 1 | grep -E "^PhysMem"  | awk '{print $2}' | numfmt --to=iec --from=si
+	output=$(top -l 1 | grep -E "^PhysMem"  | awk '{print $2}' | numfmt --to=iec --from=si)
 else
-	free -b | awk 'NR == 2 {print $3}' | numfmt --to=iec --from=si
+	output=$(free -b | awk 'NR == 2 {print $3}' | numfmt --to=iec --from=si)
 fi
+
+printf "#[fg=black,bg=%s] 󰍛 #[bg=terminal,fg=terminal] %s" "$background" "$output"
