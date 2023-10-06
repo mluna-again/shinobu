ed7b785ad6e0ba5996d844a5ac17973864c9fb66
Manuel Luna <manuellunada@gmail.com>
2023-10-06 00:11:14 -0600

    warnign

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -133,7 +133,7 @@ return {
 					opts = {}, -- merged with defaults from documentation
 				},
 				signature = {
-					enabled = true,
+					enabled = false,
 					auto_open = {
 						enabled = true,
 						trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
