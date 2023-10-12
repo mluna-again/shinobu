d2780cc926e6c9a139a5d365d966506a6d041a56
Manuel Luna <manuellunada@gmail.com>
2023-10-12 14:04:08 -0600

    vim-test bindings

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -4,7 +4,35 @@ let g:test#neovim#start_normal = 1
 ]])
 return {
 	"vim-test/vim-test",
+	dependencies = {
+		"folke/which-key.nvim",
+	},
 	event = "VeryLazy",
 	config = function()
+		local wk = require("which-key")
+
+		wk.register({
+			t = {
+				r = {
+					"<cmd>TestNearest<CR>",
+					"Run test under cursor",
+					silent = true,
+					noremap = true,
+				},
+				R = {
+					"<cmd>TestFile<CR>",
+					"Run tests in current file",
+					silent = true,
+					noremap = true,
+				},
+				T = {
+					"<cmd>TestSuite<CR>",
+					"Run all tests in current project",
+					silent = true,
+					noremap = true,
+				},
+				name = "Testing",
+			},
+		}, { prefix = "<Leader>" })
 	end,
 }
