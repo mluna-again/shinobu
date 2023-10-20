41f25a1087f74753d4e520eca05f1fc11f76d632
Manuel Luna <manuellunada@gmail.com>
2023-10-20 10:29:52 -0600

    handle ctrl-d

--- .local/scripts/pretty_hurl.fish
+++ .local/scripts/pretty_hurl.fish
@@ -22,6 +22,20 @@ function ihurl
     _print_ihurl_output
 
     while read -g -P "query> " query
+        test $status = 0; or set -l should_exit true
+        test $query = q; and set -l should_exit true
+        test $query = quit; and set -l should_exit true
+
+        if test $should_exit = true
+            if test -n "$TMUX"
+                tmux send-keys -t . C-c
+                break
+            end
+
+            talert "Without tmux you need to manually press Ctrl-c."
+            break
+        end
+
         test -z "$query"; and set -g query "."
 
         clear
@@ -31,6 +45,13 @@ function ihurl
             tmux send-keys -t . "$query"
         end
     end
+
+    # user pressed Ctrl-d
+    if test -n "$TMUX"
+        tmux send-keys -t . C-c
+        exit
+    end
+    talert "Without tmux you need to manually press Ctrl-c."
 end
 
 ihurl $argv
