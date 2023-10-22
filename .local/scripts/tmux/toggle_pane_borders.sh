199a78a781ff7b470b76ebb6654be641c15b77fa
Manuel Luna <manuellunada@gmail.com>
2023-10-22 14:27:04 -0600

    no borders on code windows

--- .local/scripts/tmux/toggle_pane_borders.sh
+++ .local/scripts/tmux/toggle_pane_borders.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-ignored_windows='["cmd","api","pgsql","dashboard"]'
+ignored_windows='["cmd","api","pgsql","dashboard","code"]'
 current_window=$(
 	tmux list-windows -F "#{window_name} #{window_active}" | \
 		awk '$2 == 1' | \
