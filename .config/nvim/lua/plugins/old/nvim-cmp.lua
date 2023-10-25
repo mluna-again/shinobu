dde74ab53f1848c22568fa3857aa810574986ea9
Manuel Luna <manuellunada@gmail.com>
2023-10-24 19:40:22 -0600

    missin file

---
+++ .config/nvim/lua/plugins/old/nvim-cmp.lua
@@ -0,0 +1,14 @@
+require("core.maps")
+
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-emoji",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"quangnguyen30192/cmp-nvim-ultisnips",
+	},
+}
