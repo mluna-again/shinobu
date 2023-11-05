84353027c6760e7cbfd03e1fa73629769f76461f
Manuel Luna <manuellunada@gmail.com>
2023-11-04 20:08:05 -0600

    my pc is getting hot

--- .local/scripts/tmux/date.sh
+++ .local/scripts/tmux/date.sh
@@ -7,4 +7,4 @@ icon_color="$2"
 
 time=$(date "+ %I:%M %p • %a %d")
 
-printf "#[fg=white,bg=terminal]%s #[fg=black,bg=%s] 󰥔 #[fg=white,bg=terminal]" "$time" "$icon_color"
+printf "#[fg=terminal,bg=terminal]%s #[fg=black,bg=%s] 󰥔 #[fg=white,bg=terminal]" "$time" "$icon_color"
