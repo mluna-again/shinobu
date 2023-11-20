537df317d48b30ca0537250deaf9fa8a097f8d45
Manuel Luna <manuellunada@gmail.com>
2023-11-20 15:15:08 -0600

    show hostname

--- .local/scripts/tmux/ssh.sh
+++ .local/scripts/tmux/ssh.sh
@@ -2,9 +2,10 @@
 
 bg="$1"
 width="$2"
+hostname=$(hostname)
 
 [ "$width" -le 100 ] && exit
 
-[ -n "$SSH_CLIENT" ] && printf " #[fg=black,bg=%s] 󰒍 #[fg=terminal,bg=terminal] SSH" "$bg"
+[ -n "$SSH_CLIENT" ] && printf " #[fg=black,bg=%s] 󰒍 #[fg=terminal,bg=terminal] %s" "$bg" "$hostname"
 
 true
