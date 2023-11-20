94a6ffc0c223527d70d7cd932a9f88bae85780db
Manuel Luna <manuellunada@gmail.com>
2023-11-20 15:31:18 -0600

    force ssh indicator

--- .local/scripts/tmux/ssh.sh
+++ .local/scripts/tmux/ssh.sh
@@ -2,10 +2,11 @@
 
 bg="$1"
 width="$2"
+force="$3"
 hostname=$(hostname)
 
 [ "$width" -le 100 ] && exit
 
-[ -n "$SSH_CLIENT" ] && printf " #[fg=black,bg=%s] 󰒍 #[fg=terminal,bg=terminal] %s" "$bg" "$hostname"
+{ [ -n "$SSH_CLIENT" ] || [ "$force" = true ] ; } && printf " #[fg=black,bg=%s] 󰒍 #[fg=terminal,bg=terminal] %s" "$bg" "$hostname"
 
 true
