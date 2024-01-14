5364118e84c934e12f0547568b8660110bfbebb1
Manuel Luna <manuellunada@gmail.com>
2024-01-14 08:06:26 -0600

    fix zoom order

--- .local/scripts/tmux/zoom.sh
+++ .local/scripts/tmux/zoom.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
-tmux resize-pane -t . -Z
-
 zoomed=$(tmux display-message -p "#{window_zoomed_flag}")
 if [ "$zoomed" -eq 1 ]; then
 	"$HOME/.local/scripts/tmux/toggle_pane_borders.sh"
 else
 	tmux set -g pane-border-status off
 fi
+
+tmux resize-pane -t . -Z
