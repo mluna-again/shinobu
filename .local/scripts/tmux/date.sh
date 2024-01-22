f3e4a6a9f9a72458ee7ac101239f8acd028e37cd
Manuel Luna <manuellunada@gmail.com>
2024-01-21 20:26:57 -0600

    so, %P is not available in osx date ig

--- .local/scripts/tmux/date.sh
+++ .local/scripts/tmux/date.sh
@@ -3,9 +3,9 @@
 width="$1"
 icon_color="$2"
 
-format="+ %I:%M %P • %a %d"
+format="+ %I:%M %p • %a %d"
 
-[ "$width" -lt 100 ] && format="+ %I:%M %P"
+[ "$width" -lt 100 ] && format="+ %I:%M %p"
 
 time=$(LC_TIME=en_US.UTF-8 date "$format")
 
