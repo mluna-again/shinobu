7efbabdb8709356de0d35757aa53346a29c770f6
Manuel Luna <manuellunada@gmail.com>
2024-01-28 21:06:04 -0600

    tmux: alice binding

---
+++ .local/scripts/tmux/alice.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+current_session=$(tmux display -p "#{session_name}")
+if [ "$current_session" = "alice" ]; then
+	tmux switch-client -l
+	exit
+fi
+
+if ! tmux has-session -t alice; then
+	tmux new-session -d -s alice -n chat
+fi
+
+tmux switch-client -t alice
+
+current_cmd=$(tmux display -p "#{pane_current_command}")
+if [ "$current_cmd" = "fish" ]; then
+	tmux send-keys -t . ask Enter
+fi
+
+true
