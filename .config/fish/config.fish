cce1a82508e3ca960693eb9468a54e952cc4af8f
Manuel Luna <manuellunada@gmail.com>
2024-02-08 14:23:39 -0600

    invert status pos when nested

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -121,10 +121,10 @@ command -v bat &>/dev/null; and function cat
 end
 
 function ssh
-    tmux set status-position bottom &>/dev/null
+    tmux set status-position top &>/dev/null
     tmux set @force_ssh_indicator true &>/dev/null
     command ssh $argv
-    tmux set status-position top &>/dev/null
+    tmux set status-position bottom &>/dev/null
     tmux set @force_ssh_indicator false &>/dev/null
 end
 
