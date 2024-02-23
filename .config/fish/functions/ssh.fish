96af1ae72a631215e3cd1f4d8065f3c880d5c06f
Manuel Luna <manuellunada@gmail.com>
2024-02-22 21:04:11 -0600

    tmux: status bar position

--- .config/fish/functions/ssh.fish
+++ .config/fish/functions/ssh.fish
@@ -1,8 +1,8 @@
 function ssh
-    tmux set status-position top &>/dev/null
+    tmux set status-position bottom &>/dev/null
     tmux set @force_ssh_indicator true &>/dev/null
     command ssh $argv
-    tmux set status-position bottom &>/dev/null
+    tmux set status-position top &>/dev/null
     tmux set @force_ssh_indicator false &>/dev/null
 end
 
