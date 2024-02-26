21022b998984926c527bd5d9af76a7e912b5435b
Manuel Luna <manuellunada@gmail.com>
2024-02-26 14:02:41 -0600

    bash: talert icons

--- .config/nvim/snippets/sh.lua
+++ .config/nvim/snippets/sh.lua
@@ -32,8 +32,8 @@ return {
 		t({
 			[[istmux() { [ -n "$TMUX" ]; }]],
 			[[talert() { tmux display -d 0 "#[bg=#{@color_info},fill=#{@color_info},fg=black] 󰭺 Message: $*"; }]],
-			[[terror() { tmux display -d 0 "#[bg=#{@color_error},fill=#{@color_error},fg=black] 󰭺 Message: $*"; }]],
-			[[tsuccess() { tmux display -d 0 "#[bg=#{@color_success},fill=#{@color_success},fg=black] 󰭺 Message: $*"; }]],
+			[[terror() { tmux display -d 0 "#[bg=#{@color_error},fill=#{@color_error},fg=black]  Message: $*"; }]],
+			[[tsuccess() { tmux display -d 0 "#[bg=#{@color_success},fill=#{@color_success},fg=black]  Message: $*"; }]],
 		})
 	})
 }, {}
