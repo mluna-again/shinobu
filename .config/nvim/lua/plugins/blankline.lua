df750a834f32798421299570be63f74606fca3f4
Manuel Luna <manuellunada@gmail.com>
2024-02-26 17:18:39 -0600

    nvim: blankline

---
+++ .config/nvim/lua/plugins/blankline.lua
@@ -0,0 +1,12 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	config = function()
+		require("ibl").setup({
+			indent = { char = "·" },
+			whitespace = {
+				remove_blankline_trail = false,
+			},
+			scope = { enabled = false },
+		})
+	end,
+}
