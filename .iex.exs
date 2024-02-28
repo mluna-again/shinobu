558ebb67d31bf4263e02e3f3fc28994295374cb0
Manuel Luna <manuellunada@gmail.com>
2024-02-27 19:36:09 -0600

    iex: neofetch

--- .iex.exs
+++ .iex.exs
@@ -29,8 +29,57 @@ if function_exported?(Mix, :target, 0) do
       "#{IO.ANSI.yellow()}(%counter) " <>
         "#{IO.ANSI.blue()}#{app_name || project_dir}" <>
         "#{IO.ANSI.red()}@" <>
-          "#{IO.ANSI.magenta()}#{local_ip}#{IO.ANSI.reset()}" <>
+        "#{IO.ANSI.magenta()}#{local_ip}#{IO.ANSI.reset()}" <>
         "$",
     continuation_prompt: "..."
   )
 end
+
+defmodule IExHelpers do
+  def neofetch do
+    """
+    #{IO.ANSI.magenta()}
+                x
+               WNX
+              Odc:xN
+            0ddko,oX                 #{IO.ANSI.yellow()}#{username()}#{IO.ANSI.blue()}@#{IO.ANSI.green()}#{path()}#{IO.ANSI.magenta()}
+           kokNWOllOW                -------------------------------
+         KdoKWMMNKxl0W               Elixir: #{IO.ANSI.yellow()}#{version()}#{IO.ANSI.magenta()}
+        0odXMMMMMMNxoON              Erlang: #{IO.ANSI.yellow()}#{erl_version()}#{IO.ANSI.magenta()}
+       0lxNMMMMMMMMW0dd0N
+      0oxNMMMMMMMMMMMNOodKW
+      odXMMMMMMMMMMMMMMXxokN
+     xl0MMMMMMMMMMMMMMMMW0odX
+    xoxWMMMMMMMMMMMMMMMMMMKodN
+    0lOMMMMMMMMMMMMMMMMMMMWOlO
+    OlOMWKXMMMMMMMMMMMMMMMMKlxW
+    KlxWXodNMMMMMMMMMMMMMMM0lkW
+    xxoKWOlkNMMMMMMMMMMMMMWkl0
+     XooKN0ddkKNWWWMMMMMMWOlkW
+      XxokXN0kxxkkKMMMMN0doON
+       WKxdxk0KKKKXK0OxddkXW
+         WNKOxxxxxxxxkOXW
+             WWWWWWW
+    """
+    |> IO.puts()
+  end
+
+  defp username do
+    System.get_env("USER")
+  end
+
+  defp path do
+    {:ok, cwd} = File.cwd()
+    Path.basename(cwd)
+  end
+
+  defp version do
+    System.version()
+  end
+
+  defp erl_version do
+    "OTP #{:erlang.system_info(:otp_release)}"
+  end
+end
+
+import IExHelpers
