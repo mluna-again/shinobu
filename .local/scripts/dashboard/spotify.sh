27d7dbde01ef38eff3d07953da3f7be51bb1c7df
Manuel Luna <manuellunada@gmail.com>
2024-03-02 22:49:42 -0600

    dash: refactor spotify script

--- .local/scripts/dashboard/spotify.sh
+++ .local/scripts/dashboard/spotify.sh
@@ -1,23 +1,44 @@
 #! /usr/bin/env bash
 
-BOP_URL="http://localhost:8888"
+declare mode="${1:-tmux}"
+declare BOP_URL="http://localhost:8888"
+
+: # bash 5+ required
+# shellcheck disable=SC2120
+die() {
+	[ -n "$*" ] && tostderr "$*"
+	exit 1
+}
+info() { printf "%s\n" "$*"; }
+tostderr() {
+	tput setaf 1 && printf "%s@%s: %s\n" "$0" "${BASH_LINENO[-2]}" "$*" >&2
+	tput sgr0
+}
+assert_installed() { command -v "$1" &>/dev/null || die "$1 is not installed."; }
+assert_not_empty() { [ -z "${!1}" ] && die "$1 is empty when it shouldn't be."; }
+broken_pipe() { grep -vq "^[0 ]*$" <<<"${PIPESTATUS[*]}"; }
 
 _display_bop_dead_message() {
-  local msg
-  msg=${1:-"bop is asleep!"}
-  tmux display -d 0 "#[bg=red,fill=red,fg=black] 󰭺 Message: $msg"
-  exit 1
+	local msg
+	msg=${1:-"bop is asleep!"}
+
+	if [ "$mode" = tmux ]; then
+		tmux display -d 0 "#[bg=red,fill=red,fg=black] 󰭺 Message: $msg"
+	else
+		tostderr "$msg"
+	fi
+	exit 1
 }
 
 status=$(curl -sSf "$BOP_URL/status" 2>&1)
-if grep -i "connection refused" <<< "$status"; then
-  _display_bop_dead_message "bop is offline."
-  exit 1
+if grep -iq "connection refused" <<<"$status"; then
+	_display_bop_dead_message "bop is offline."
+	exit 1
 fi
 
-if grep -i "404" <<< "$status"; then
-  _display_bop_dead_message "no music playing right now."
-  exit 1
+if grep -iq "404" <<<"$status"; then
+	_display_bop_dead_message "no music playing right now."
+	exit 1
 fi
 
 DELAY=7
@@ -40,7 +61,7 @@ command -v jq &>/dev/null || {
 
 _ellipsis() {
 	read -r text
-	awk '{printf substr($0, 0, 29); if (length($0) > 29) { printf "..."; } printf "\n"}' <<< "$text"
+	awk '{printf substr($0, 0, 29); if (length($0) > 29) { printf "..."; } printf "\n"}' <<<"$text"
 }
 
 date() {
@@ -55,7 +76,7 @@ download_if_not_exists() {
 	url="$1"
 	[ -z "$url" ] && return
 
-	hash=$(sha256sum <<< "$url" | awk '{print $1}')
+	hash=$(sha256sum <<<"$url" | awk '{print $1}')
 	image_path="$CURRENT_PATH/downloads/$hash"
 	printf "%s\n" "$image_path"
 	find "$CURRENT_PATH/downloads" -type f -iname "$hash\.*" | grep -q . && return
@@ -71,7 +92,7 @@ chafa_if_not_yet() {
 
 	[ -e "$path" ] && return
 
-	chafa -s 30x30 "${path}.jpg" > "$path"
+	chafa -s 30x30 "${path}.jpg" >"$path"
 }
 
 progress_bar() {
@@ -82,18 +103,18 @@ progress_bar() {
 	total="$2"
 	# im going to jail for this one
 	percentage=$(jq -n "(${current}/${total}) * 100 | round")
-	steps=$(( percentage / 10 ))
+	steps=$((percentage / 10))
 
 	start=$(date +"%M:%S" -d "1970-01-01 $current seconds")
 	ending=$(date +"%M:%S" -d "1970-01-01 $total seconds")
 
 	printf "%s " "$start"
-	for (( i=0; i<10; i++ )); do
-		(( i == 0 )) && {
+	for ((i = 0; i < 10; i++)); do
+		((i == 0)) && {
 			printf '\e[33m█\033[0m'
 			continue
 		}
-		(( i <= steps )) && {
+		((i <= steps)) && {
 			printf '\e[33m██\033[0m'
 			continue
 		}
@@ -107,43 +128,43 @@ current_song=$(curl -fs "$BOP_URL/status")
 	_display_bop_dead_message
 	exit 1
 }
-grep -iq "Not found" <<< "$current_song" && {
+grep -iq "Not found" <<<"$current_song" && {
 	printf "No song playing.\n"
 	exit
 }
 
-song=$(jq -r '.display_name' <<< "$current_song" | _ellipsis)
-album=$(jq -r '.album' <<< "$current_song" | _ellipsis)
-artist=$(jq -r '.artist' <<< "$current_song" | _ellipsis)
-image=$(jq -r '.image_url' <<< "$current_song")
-is_playing=$(jq -r '.is_playing' <<< "$current_song")
-current_time=$(jq -r '.current_second' <<< "$current_song")
-total_time=$(jq -r '.total_seconds' <<< "$current_song")
+song=$(jq -r '.display_name' <<<"$current_song" | _ellipsis)
+album=$(jq -r '.album' <<<"$current_song" | _ellipsis)
+artist=$(jq -r '.artist' <<<"$current_song" | _ellipsis)
+image=$(jq -r '.image_url' <<<"$current_song")
+is_playing=$(jq -r '.is_playing' <<<"$current_song")
+current_time=$(jq -r '.current_second' <<<"$current_song")
+total_time=$(jq -r '.total_seconds' <<<"$current_song")
 
 image_path=$(download_if_not_exists "$image")
 chafa_if_not_yet "$image_path"
 
 refetch_data() {
 	current_song=$(curl -fs "$BOP_URL/status")
-	grep -iq "Not found" <<< "$current_song" && {
+	grep -iq "Not found" <<<"$current_song" && {
 		printf "No song playing.\n"
 		exit
 	}
-	song=$(jq -r '.display_name' <<< "$current_song" | _ellipsis)
-	album=$(jq -r '.album' <<< "$current_song" | _ellipsis)
-	artist=$(jq -r '.artist' <<< "$current_song" | _ellipsis)
-	image=$(jq -r '.image_url' <<< "$current_song")
-	current_time=$(jq -r '.current_second' <<< "$current_song")
-	total_time=$(jq -r '.total_seconds' <<< "$current_song")
-	is_playing=$(jq -r '.is_playing' <<< "$current_song")
+	song=$(jq -r '.display_name' <<<"$current_song" | _ellipsis)
+	album=$(jq -r '.album' <<<"$current_song" | _ellipsis)
+	artist=$(jq -r '.artist' <<<"$current_song" | _ellipsis)
+	image=$(jq -r '.image_url' <<<"$current_song")
+	current_time=$(jq -r '.current_second' <<<"$current_song")
+	total_time=$(jq -r '.total_seconds' <<<"$current_song")
+	is_playing=$(jq -r '.is_playing' <<<"$current_song")
 
 	image_path=$(download_if_not_exists "$image")
 	chafa_if_not_yet "$image_path"
 }
 
 lines=$(tput lines)
-lines=$(( (lines - 15) / 2 )) # 15 is the size of the album cover
-horizontal_padd=$(( ($(tput cols) - 60) / 2 ))
+lines=$(((lines - 15) / 2)) # 15 is the size of the album cover
+horizontal_padd=$((($(tput cols) - 60) / 2))
 
 time_since_last_fetch=0
 
@@ -151,11 +172,11 @@ printf '\033[?25l'
 trap "printf '\033[?25h' ; trap - SIGTERM SIGINT ; exit" SIGTERM SIGINT
 
 while true; do
-	(( current_time >= total_time )) && {
+	((current_time >= total_time)) && {
 		time_since_last_fetch=0
 		refetch_data || break
 	}
-	(( time_since_last_fetch > "$DELAY" )) && {
+	((time_since_last_fetch > "$DELAY")) && {
 		time_since_last_fetch=0
 		refetch_data || break
 	}
@@ -167,10 +188,10 @@ while true; do
 	for line in $(cat "$image_path"); do
 		[ $index -eq 0 ] && {
 			clear
-			[ "$CENTER" = true ] && for (( i=0;i<lines;i++ )); do printf "\n"; done
+			[ "$CENTER" = true ] && for ((i = 0; i < lines; i++)); do printf "\n"; done
 		}
 
-		for (( i=0; i<horizontal_padd; i++ )); do printf " "; done
+		for ((i = 0; i < horizontal_padd; i++)); do printf " "; done
 
 		printf "%s" "$line"
 
@@ -184,12 +205,12 @@ while true; do
 		index=$((index + 1))
 	done
 
-	current_time=$(( current_time + 1 ))
-	time_since_last_fetch=$(( time_since_last_fetch + 1 ))
+	current_time=$((current_time + 1))
+	time_since_last_fetch=$((time_since_last_fetch + 1))
 
 	[ "$is_playing" = false ] && {
 		sleep "$DELAY"
-		time_since_last_fetch=$(( time_since_last_fetch + DELAY ))
+		time_since_last_fetch=$((time_since_last_fetch + DELAY))
 		continue
 	}
 
