9586250b4fe6c3cb972e84bcc6c900f84ecd8057
Manuel Luna <manuellunada@gmail.com>
2024-03-27 22:35:20 -0600

    tmux: search color

--- .local/scripts/tmux/search_all_panes.sh
+++ .local/scripts/tmux/search_all_panes.sh
@@ -5,7 +5,7 @@ pane=$(while read -r pane; do
 	name=$(awk '{print $2}' <<< "$pane")
 
 	tmux capture-pane -S -1000 -p -t "$id" | awk "\$0 != \"\" {printf \"%s %s %s: %s\n\", \"$id\", NR, \"$name\", \$0 ;}"
-done < <(tmux list-panes -a -F "#{pane_id} #{window_name}") | fzf --cycle -i -e --preview="tmux capture-pane -p -S -1000 -t {1} | grep -i --color=always -C 30 "{q}"")
+done < <(tmux list-panes -a -F "#{pane_id} #{window_name}") | fzf --cycle -i -e --preview="tmux capture-pane -e -p -S -1000 -t {1} | grep -i --color=always -C 15 "{q}"")
 
 [ -z "$pane" ] && exit
 
