890fc97f1cc672ce7de2920f977a0b0d0d00b860
Manuel Luna <manuellunada@gmail.com>
2024-03-27 22:32:40 -0600

    tmux: forgot the most important thing

--- .local/scripts/tmux/search_all_panes.sh
+++ .local/scripts/tmux/search_all_panes.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/env bash
 
-while read -r pane; do
+pane=$(while read -r pane; do
 	id=$(awk '{print $1}' <<< "$pane")
 	name=$(awk '{print $2}' <<< "$pane")
 
 	tmux capture-pane -S -1000 -p -t "$id" | awk "\$0 != \"\" {printf \"%s %s %s: %s\n\", \"$id\", NR, \"$name\", \$0 ;}"
-done < <(tmux list-panes -a -F "#{pane_id} #{window_name}") | fzf --cycle -i -e --preview="tmux capture-pane -p -S -1000 -t {1} | grep -i --color=always -C 30 "{q}""
+done < <(tmux list-panes -a -F "#{pane_id} #{window_name}") | fzf --cycle -i -e --preview="tmux capture-pane -p -S -1000 -t {1} | grep -i --color=always -C 30 "{q}"")
+
+[ -z "$pane" ] && exit
+
+pane_id=$(awk '{print $1}' <<< "$pane")
+tmux switch-client -t "$pane_id"
