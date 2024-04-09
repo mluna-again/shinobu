776ef6c0e49dfc276676c9201c334e04c212ba3f
Manuel Luna <manuellunada@gmail.com>
2024-04-08 21:07:43 -0600

    dotsL, gL: diff

--- .config/fish/functions/dotsL.fish
+++ .config/fish/functions/dotsL.fish
@@ -3,9 +3,13 @@ function dotsL
     set -l fsize (math \($cols - 10\) / 2)
     set -l fsize (math floor $fsize)
 
-    set -l log (yadm log --format="%h • %s • %an" | fzf --header="Search git logs" --preview="echo {} | fold -w $fsize -s && yadm diff {+1}^ {+1} | delta")
+    set -l log (yadm log --format="%h • %s • %an @ %ad" | fzf --header="Search git logs" --preview="echo {} | fold -w $fsize -s && yadm diff '{+1}~' {+1} | delta")
 
     test -z "$log"; and return
 
-    yadm log -1 (echo "$log" | awk '{ print $1 }')
+    set -l log_id (echo "$log" | awk '{ print $1 }')
+    yadm log -1 "$log_id"
+    yadm diff "$log_id~" "$log_id"
+
+    echo -n "$log_id" | fish_clipboard_copy
 end
--- .config/fish/functions/gL.fish
+++ .config/fish/functions/gL.fish
@@ -3,9 +3,12 @@ function gL
     set -l fsize (math \($cols - 10\) / 2)
     set -l fsize (math floor $fsize)
 
-    set -l log (git log --format="%h • %s • %an" | fzf --header="Search git logs" --preview="echo {} | fold -w $fsize -s && git diff {+1}^ {+1} | delta")
+    set -l log (git log --format="%h • %s • %an @ %ad" | fzf --header="Search git logs" --preview="echo {} | fold -w $fsize -s && git diff '{+1}~' {+1} | delta")
 
     test -z "$log"; and return
 
-    git log -1 (echo "$log" | awk '{ print $1 }')
+    set -l log_id (echo "$log" | awk '{ print $1 }')
+    git log -1 "$log_id"
+    git diff "$log_id~" "$log_id"
+    echo -n "$log_id" | fish_clipboard_copy
 end
