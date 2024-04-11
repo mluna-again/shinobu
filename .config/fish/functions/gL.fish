fcf678ded334a711b41b44f547d8d124ba5c1bd3
Manuel Luna <manuellunada@gmail.com>
2024-04-10 18:56:30 -0600

    fzf_git: dont sort

--- .config/fish/functions/gL.fish
+++ .config/fish/functions/gL.fish
@@ -3,7 +3,7 @@ function gL
     set -l fsize (math \($cols - 10\) / 2)
     set -l fsize (math floor $fsize)
 
-    set -l log (git log --format="%h • %s • %an @ %ad" | fzf --header="Search git logs" --preview="echo {} | fold -w $fsize -s && git diff '{+1}~' {+1} | delta")
+    set -l log (git log --format="%h • %s • %an @ %ad" | fzf +s -i -e --header="Search git logs" --preview="echo {} | fold -w $fsize -s && git diff '{+1}~' {+1} | delta")
 
     test -z "$log"; and return
 
