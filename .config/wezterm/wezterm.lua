6384ee091de8861cae281aea5cc8cc1b6423867f
Manuel Luna <manuellunada@gmail.com>
2024-05-01 20:14:04 -0600

    wezterm: initial config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,38 @@
+local kanagawa = require("kanagawa")
+local w = require("wezterm")
+
+local config = {}
+
+config.font = w.font("FiraCode Nerd Font")
+config.font_size = 14
+
+config.colors = kanagawa
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  bottom = 0,
+  top = 0
+}
+
+-- config.window_background_image = string.format("%s/.local/walls/valhalla.gif", os.getenv("HOME"))
+config.window_background_image_hsb = {
+  brightness = 0.02
+}
+
+config.keys = {
+  {
+    key = "f",
+    mods = "CMD",
+    action = w.action.ToggleFullScreen
+  },
+  {
+    key = "f",
+    mods = "CTRL",
+    action = w.action.ToggleFullScreen
+  }
+}
+
+return config
