80c537ba039a2ba1e0c1a4fd5733c33094c1fe8f
Manuel Luna <manuellunada@gmail.com>
2024-05-28 00:35:05 -0600

    nvim: fix sign_define deprecated warning

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -62,10 +62,22 @@ vim.filetype.add({ extension = {
 	heex = "html",
 } })
 
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+			[vim.diagnostic.severity.INFO] = "",
+		},
+		linehl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+		},
+	},
+})
 
 vim.api.nvim_create_user_command("Lsp", function()
 	vim.cmd("Lazy load lsp-stuff")
