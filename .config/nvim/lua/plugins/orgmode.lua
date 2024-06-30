1cf79e61d6d36119b4b02835c4b2c2e981f07b46
Manuel Luna <manuellunada@gmail.com>
2024-06-30 02:27:44 -0600

    nvim: org mappings

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -1,16 +1,25 @@
 return {
   'nvim-orgmode/orgmode',
-  event = 'VeryLazy',
+  dependencies = { "folke/which-key.nvim" },
   ft = { 'org' },
   config = function()
     require('orgmode').setup({
       -- org_agenda_files = '~/orgfiles/**/*',
       -- org_default_notes_file = '~/orgfiles/refile.org',
+      org_todo_keywords = { "TODO", "DOING", "|", "DONE" },
+      org_startup_folded = "showeverything",
       mappings = {
         org = {
-          org_toggle_checkbox = "<leader>oo"
+          org_toggle_checkbox = "<leader>o<leader>",
+          org_timestamp_up = "<leader>o>",
+          org_timestamp_down = "<leader>o<",
         }
       }
     })
+
+    local wk = require("which-key")
+    wk.register({
+      name = "Orgmode"
+    }, { prefix = "<leader>o" })
   end,
 }
