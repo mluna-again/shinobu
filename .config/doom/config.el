dd4871c26a2a60f437b03c1fa5ac13a3d9ece273
Manuel Luna <manuellunada@gmail.com>
2024-07-28 15:27:41 -0600

    emacs: gruvbox

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -34,7 +34,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-monokai-classic)
+(setq doom-theme 'doom-gruvbox)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -173,3 +173,6 @@
                :key "SPC o c"
                :icon (nerd-icons-sucicon "nf-seti-config" :face 'doom-dashboard-menu-title)
                :action open-config-file))
+
+(custom-theme-set-faces! 'doom-gruvbox
+  '(default :background "#181816"))
