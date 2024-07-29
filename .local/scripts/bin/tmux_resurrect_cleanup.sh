858f2acd07fb97f886f5fdc55660444386c693d6
Manuel Luna <manuellunada@gmail.com>
2024-07-29 09:28:30 -0600

    tmux: resurrect cleanup script, rollback opt

--- .local/scripts/bin/tmux_resurrect_cleanup.sh
+++ .local/scripts/bin/tmux_resurrect_cleanup.sh
@@ -5,6 +5,10 @@ if [ "$1" = help ] || [ "$1" = -h ] || [ "$1" = --help ]; then
 Sometimes tmux-resurrect doesn't feel like working, for some reason the last session points to a file that
 doesn't exist. This script re-creates the link to the last existing session. If it doesn't work the first
 time maybe run it once more, that'll surely fix it :D (it won't).
+
+Usage:
+tmux_resurrect_cleanup.sh             # re-links the last session file
+tmux_resurrect_cleanup.sh rollback    # deletes the latest session file, and re-links the before last session file
 EOF
   exit 1
 fi
@@ -14,6 +18,16 @@ CACHE_DIR="$HOME/.cache/resurrect"
 
 cd "$CACHE_DIR" || exit
 last_file=$(find . -printf "%T@ %Tc %p\n" | sort -n | head -1 | awk '{print $NF}' | sed 's|./||')
+
+if [ ! -f "$last_file" ]; then
+  >&2 echo no session found
+  exit 1
+fi
+
+if [ "$1" = rollback ]; then
+  rm "$CACHE_DIR/$last_file"
+  last_file=$(find . -printf "%T@ %Tc %p\n" | sort -n | head -1 | awk '{print $NF}' | sed 's|./||')
+fi
 
 if [ ! -f "$last_file" ]; then
   >&2 echo no session found
