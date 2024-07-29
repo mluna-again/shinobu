81be9e042af408853de6776f407f2f34c986efd3
Manuel Luna <manuellunada@gmail.com>
2024-07-28 19:59:17 -0600

    sway: toggle statusbar command

--- .local/scripts/swayutils/command-palette.sh
+++ .local/scripts/swayutils/command-palette.sh
@@ -1,7 +1,15 @@
 #! /usr/bin/env bash
 
+exit_with_error() {
+  local msg="$1"
+
+   notify-send -t 5000 -u critical "$msg"
+  exit 1
+}
+
 option=$(cat - <<EOF | bemenu -i --list 50
 Open book
+Toggle statusbar
 EOF
 )
 
@@ -10,6 +18,10 @@ case "$option" in
     book=$(find "$HOME/Books" -type f -iname "*.pdf" | sed "s|$HOME/Books/||" | bemenu -i --list 50) || exit
     [ -z "$book" ] && exit
     xdg-open "$HOME/Books/$book"
+    ;;
+
+  "Toggle statusbar")
+    swaymsg bar mode toggle || exit_with_message "Failed to toggle statusbar"
     ;;
 
   *)
