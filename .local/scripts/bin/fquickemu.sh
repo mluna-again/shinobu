4dfdf95bbcc9f9d9aeab6443aa2d7620de9769f3
Manuel Luna <manuellunada@gmail.com>
2024-08-08 16:50:19 -0600

    fquickemu.sh: multi-mode

--- .local/scripts/bin/fquickemu.sh
+++ .local/scripts/bin/fquickemu.sh
@@ -1,33 +1,83 @@
 #! /usr/bin/env bash
 
-CREDS="${1:-user@localhost}"
+CREDS="user@localhost"
+DISPLAY_MODE=none
+RUN_MODE=run
 
-vm=$(find . -maxdepth 1 -iname "*.conf" | fzf --header="Available configurations") || exit
+while true; do
+	[ -z "$1" ] && break
 
-[ -z "$vm" ] && exit
+	case "$1" in
+		--display)
+			DISPLAY_MODE=spice
+			shift
+			;;
 
-conf_dir=$(sed 's|.conf$||' <<< "$vm") || exit
-port=$(awk -F, '/ssh/ {print $2}' < "$conf_dir/${conf_dir}.ports") || exit
+		--kill)
+			RUN_MODE=kill_vm
+			shift
+			;;
 
-if [ -z "$port" ]; then
-	2>&1 echo "could not find SSH port!"
-	exit 1
-fi
+		*)
+			CREDS="$1"
+			shift
+			;;
+	esac
+done
 
-if [[ ! "$port" =~ [0-9]+ ]]; then
-	2>&1 echo "port found is in invalid format: $port!"
-	exit 1
-fi
+run() {
+	vm=$(find . -maxdepth 1 -iname "*.conf" | fzf --header="Available configurations") || exit
 
-quickemu --vm "$vm" --display none || exit
+	[ -z "$vm" ] && exit
 
-if ! ssh -o "UserKnownHostsFile=/dev/null" -p "$port" "$CREDS"; then
-	2>&1 echo "ssh failed. retrying..."
-	sleep 3
+	conf_dir=$(sed 's|.conf$||' <<< "$vm") || exit
+	port=$(awk -F, '/ssh/ {print $2}' < "$conf_dir/${conf_dir}.ports") || exit
 
-	if ! ssh -o "UserKnownHostsFile=/dev/null" -p "$port" "$CREDS"; then
-		2>&1 echo "failed to ssh with args: $CREDS"
-		2>&1 echo "you can pass different args with: fquickemu.sh user@localhost"
+	if [ -z "$port" ]; then
+		2>&1 echo "could not find SSH port!"
 		exit 1
 	fi
-fi
+
+	if [[ ! "$port" =~ [0-9]+ ]]; then
+		2>&1 echo "port found is in invalid format: $port!"
+		exit 1
+	fi
+
+	quickemu --vm "$vm" --display "$DISPLAY_MODE" || exit
+
+	if [ "$DISPLAY_MODE" = none ]; then
+		if ! ssh -o "UserKnownHostsFile=/dev/null" -p "$port" "$CREDS"; then
+			2>&1 echo "ssh failed. retrying..."
+			sleep 3
+
+			if ! ssh -o "UserKnownHostsFile=/dev/null" -p "$port" "$CREDS"; then
+				2>&1 echo "failed to ssh with args: $CREDS"
+				2>&1 echo "you can pass different args with: fquickemu.sh user@localhost"
+				exit 1
+			fi
+		fi
+	fi
+}
+
+kill_vm() {
+	vm=$(find . -maxdepth 1 -iname "*.conf" | fzf --header="Available configurations") || exit
+
+	[ -z "$vm" ] && exit
+
+	quickemu --vm "$vm" --kill
+}
+
+case "$RUN_MODE" in
+	run)
+		run
+		;;
+
+	kill_vm)
+		kill_vm
+		;;
+
+	*)
+		2>&1 echo "invalid mode: $RUN_MODE"
+		exit 1
+		;;
+esac
